dd0ef2a18eb42d9d83e655a796489623c76cd05a
fcamblor <frederic.camblor@4sh.fr>
2014-05-02 16:57:03 +0200

    Avoiding git rm with no param + fixed spaces in path params

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git clone -b master git@github.com:bdxio/bdxio.github.io.git dist/
 cd dist
 # Adding files, removing missing files (including files with spaces as well)
 git add .
-git rm $(git ls-files --deleted | sed -e 's/^/"/g' -e 's/$/"/g')
+if [ $(git ls-files --deleted | wc -l) -ne 0 ]; then git ls-files --deleted | sed -e 's/^/"/g' -e 's/$/"/g' | xargs git rm; fi;
 git commit -m "Auto-deploy"
 
 # Pushing to master branch, which is sync-ed with www.bdx.io
