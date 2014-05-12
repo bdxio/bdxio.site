ab14121fd4afb9c58f6ecf1df1be502814790f33
fcamblor <frederic.camblor@4sh.fr>
2014-05-12 12:09:53 +0200

    fixed useless comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -378,7 +378,7 @@ module.exports = function (grunt) {
             grunt.task.run([
                 'clean:server',
                 'concurrent:test',
-                'autoprefixer',
+                'autoprefixer'
             ]);
         }
 
