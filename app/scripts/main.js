5bdc075e82c8f67d3cc0f6290eec41926ef2d6f9
fcamblor <frederic.camblor@4sh.fr>
2014-07-24 21:58:18 +0200

    angularjs-ified website

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('Hello Bordeaux !');
+
+angular.module('bdxioModule', [])
+.run(function($rootScope, $location, $anchorScroll) {
+    $rootScope.scrollTo = function(targetAnchorName) {
+        $location.hash(targetAnchorName);
+        $anchorScroll();
+    };
+    $rootScope.genMailTo = function(name) {
+        return "mailto:"+name+"@bdx.io";
+    };
+});
