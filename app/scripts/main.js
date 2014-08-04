7391deef4d8ae8a07938dfdf3fd9a788536f247c
fcamblor <frederic.camblor@4sh.fr>
2014-08-04 02:04:43 +0200

    When no anchor target is provided, removing the previous anchor target + fixed an issue related to scroll top when we have a header (see angular issue #2070)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,13 +13,22 @@ bdxioModule.config(function($routeProvider) {
         })
 });
 
-bdxioModule.run(function($rootScope, $location, $anchorScroll) {
+bdxioModule.run(function($rootScope, $location, $anchorScroll, $timeout) {
     $rootScope.goto = function(path, targetAnchorName) {
         $location.path(path);
 
         if(targetAnchorName) {
             $location.hash(targetAnchorName);
             $anchorScroll();
+
+            // Hackish here... allows to anchorscroll and take into consideration menu header
+            // because of https://github.com/angular/angular.js/issues/2070
+            $timeout(function(){
+                var bodyPaddingTop = $("body").css("padding-top");
+                scrollBy(0, -parseInt(bodyPaddingTop, 10));
+            }, 100);
+        } else {
+            $location.hash("");
         }
     };
     $rootScope.genMailTo = function(name) {
