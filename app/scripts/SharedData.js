558b66204a138cd51eda64e0cf7233de85496877
fcamblor <frederic.camblor@4sh.fr>
2014-09-06 11:09:51 +0200

    Abstracted a bit SharedData to handle easily multiple spreadsheets loaded

--- app/scripts/SharedData.js
+++ app/scripts/SharedData.js
@@ -2,22 +2,26 @@
 
 angular.module('bdxioModule').factory('SharedData', function ($q, $http, SpreadsheetReader) {
     var SharedData = {
-        DESCRIPTORS: {
-            orgas: {
-                "firstRow": 2,
-                "title": "L'équipe d'organisation",
-                "anchor": "orgas",
-                "columnFields": {
-                    "A": "firstName",
-                    "B": "lastName",
-                    "C": "bio",
-                    "D": "avatarUrl",
-                    "E": "twitter",
-                    "F": "linkedin",
-                    "G": "gplus"
+        SPREADSHEETS: [
+            {
+                tabId: 1,
+                dataField: "orgas",
+                descriptor: {
+                    "firstRow": 2,
+                    "title": "L'équipe d'organisation",
+                    "anchor": "orgas",
+                    "columnFields": {
+                        "A": "firstName",
+                        "B": "lastName",
+                        "C": "bio",
+                        "D": "avatarUrl",
+                        "E": "twitter",
+                        "F": "linkedin",
+                        "G": "gplus"
+                    }
                 }
             }
-        },
+        ],
         _data: {},
         _dataLoadedDefer: $q.defer(),
         init: function(opts) {
@@ -30,16 +34,18 @@ angular.module('bdxioModule').factory('SharedData', function ($q, $http, Spreads
                     :"https://spreadsheets.google.com/feeds/cells/1TWnEIQScabIWWGbH5GYLPeWZaM6y3Qww9aVZj6o6Xm4/"+tabId+"/public/basic?alt=json&callback=JSON_CALLBACK&v=3.0";
             };
 
-            $q.when(
-                $http.jsonp(urlFactory(1),{})
-            ).then(function(spreadsheetQueryResult){
-                $q.all([
-                    SpreadsheetReader.read(spreadsheetQueryResult.data, SharedData.DESCRIPTORS.orgas)
-                ]).then(function(results) {
-                    self._data["orgas"] = results[0];
-                    self._dataLoadedDefer.resolve();
-                    defer.resolve();
+            var fetchPromises = _.map(SharedData.SPREADSHEETS, function(spreadsheet){
+                return $http.jsonp(urlFactory(spreadsheet.tabId),{}).then(function(spreadsheetQueryResult){
+                    return SpreadsheetReader.read(spreadsheetQueryResult.data, spreadsheet.descriptor);
                 });
+            });
+
+            $q.all(fetchPromises).then(function(spreadsheetInfos) {
+                _.each(spreadsheetInfos, function(spreadsheetInfo, idx){
+                    self._data[SharedData.SPREADSHEETS[idx].dataField] = spreadsheetInfo;
+                });
+                self._dataLoadedDefer.resolve();
+                defer.resolve();
             });
 
             return defer.promise;
