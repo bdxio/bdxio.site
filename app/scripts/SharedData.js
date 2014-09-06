eb1242ae9a470e35eb1785eacafe3d8048c84464
fcamblor <frederic.camblor@4sh.fr>
2014-09-06 10:09:19 +0200

    Added lodash dependency

---
+++ app/scripts/SharedData.js
@@ -0,0 +1,13 @@
+"use strict";
+
+angular.module('bdxioModule').factory('SpreadsheetReader', function ($q) {
+    var SpreadsheetReader = {
+        read: function(spreadsheetRepresentation, descriptor) {
+
+        }
+    };
+    return SpreadsheetReader;
+});
+
+
+https://spreadsheets.google.com/feeds/cells/1TWnEIQScabIWWGbH5GYLPeWZaM6y3Qww9aVZj6o6Xm4/1/public/basic?alt=json&callback=myFunc&v=3.0
