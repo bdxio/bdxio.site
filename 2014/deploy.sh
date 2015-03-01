024e320bf0f8e344e3e0208fd21476405974ea11
Elian ORIOU <elian.oriou@4sh.fr>
2015-03-01 16:18:44 +0100

    Add version info on auto-deployment commit message

--- 2014/deploy.sh
+++ 2014/deploy.sh
@@ -19,7 +19,7 @@ cd dist
 # Adding files, removing missing files (including files with spaces as well)
 git add .
 if [ $(git ls-files --deleted | wc -l) -ne 0 ]; then git ls-files --deleted | sed -e 's/^/"/g' -e 's/$/"/g' | xargs git rm; fi;
-git commit -m "Auto-deploy"
+git commit -m "Auto-deploy - ${PWD##*/}"
 
 # Pushing to master branch, which is sync-ed with www.bdx.io ... only if there are more than 10 files in current directory
 if [ $(ls -al | wc -l) -ge 10 ]; then git push origin master; else echo "Avoiding any push since there is an empty dist/ dir"; fi;
