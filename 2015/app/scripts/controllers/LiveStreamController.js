b1b747f03e232b1ed2bf26363cf173cbb669deec
fcamblor <frederic.camblor@4sh.fr>
2015-10-19 13:58:17 +0200

    Removed livestream section from website

--- 2015/app/scripts/controllers/LiveStreamController.js
+++ 2015/app/scripts/controllers/LiveStreamController.js
@@ -3,12 +3,14 @@
 angular.module('bdxio.app')
     .controller('LiveStreamController', function ($scope, $routeParams, $sce) {
 
+    var youtubeId = _.pluck(SharedData.data("livestreams"), "youtubId");
+    // TODO: Externalize this into google spreadsheets
     var youtubeId = {
-        'gdAmphi': 'V5J4eQZ45L0',
-        'amphiA': 'P0QCauLihB0',
-        'amphiB': '7sALkjzfgPE',
-        'amphiD': 'OvYrBd8FiEE',
-        'amphiE': 'dP6VuDinFww'
+        'gdAmphi': 'xxx',
+        'amphiA': 'xxx',
+        'amphiB': 'xxx',
+        'amphiD': 'xxx',
+        'amphiE': 'xxx'
     };
 
     $scope.amphiId = $routeParams.amphiId || 'gdAmphi';
