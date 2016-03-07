9843dd300d71d93bb6effc47bbb571146cbbb170
robinlopez <robin.lopez@4sh.fr>
2016-03-07 20:32:08 +0100

    Adjustments newsletter and list networks in footer

--- 2016/src/modules/application/angular/components/AppFooterComponent.ts
+++ 2016/src/modules/application/angular/components/AppFooterComponent.ts
@@ -4,7 +4,7 @@ export class AppFooterComponent implements ng.IDirective {
          <div class="row">
                 <div class="col-sm-12 col-md-8 element-centered">
                     <div class="row sign-newsletter text-right">
-                        <span class="col-xs-12 col-md-4 col-lg-4 mb-space-bottom-15">
+                        <span class="col-xs-12 col-md-4 col-lg-4 mb-text-center mb-space-bottom-15">
                             <h6 class="label text-white font-bold">Vous souhaitez être tenu(e) informé(e) ?</h6>
                             <h6 class="label highlight-text-italic">Inscrivez-vous à la newsletter</h6>
                         </span>
@@ -13,7 +13,7 @@ export class AppFooterComponent implements ng.IDirective {
                             <input class="input-newsletter" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
                         </span>
 
-                          <span class="col-xs-12 col-md-3 col-lg-4 text-left mb-text-right space-top-2">
+                          <span class="col-xs-12 col-md-3 col-lg-4 text-left mb-text-center space-top-2">
                              <button class="btn btn-primary btn-md has-icon-left" disabled><i class="fa fa-pencil"></i>S’inscrire</button>
                         </span>
                     </div>
