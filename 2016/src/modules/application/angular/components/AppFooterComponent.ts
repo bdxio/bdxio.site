79bf4a79384b559b99d55d969b2d60690983a44c
robinlopez <robin.lopez@4sh.fr>
2016-03-09 13:55:48 +0100

    Fixze size typo newsletter footer

--- 2016/src/modules/application/angular/components/AppFooterComponent.ts
+++ 2016/src/modules/application/angular/components/AppFooterComponent.ts
@@ -5,8 +5,8 @@ export class AppFooterComponent implements ng.IDirective {
                 <div class="col-sm-12 col-md-8 element-centered">
                     <div class="row sign-newsletter text-right">
                         <span class="col-xs-12 col-md-4 col-lg-4 mb-text-center mb-space-bottom-15">
-                            <h6 class="label text-white font-bold">Vous souhaitez être tenu(e) informé(e) ?</h6>
-                            <h6 class="label highlight-text-italic">Inscrivez-vous à la newsletter</h6>
+                            <h5 class="label-newsletter text-white font-bold">Vous souhaitez être tenu(e) informé(e) ?</h5>
+                            <h4 class="label-newsletter highlight-text-italic">Inscrivez-vous à la newsletter</h4>
                         </span>
 
                          <span class="col-xs-12 col-md-5 col-lg-4 space-top-2 mb-space-bottom-15">
