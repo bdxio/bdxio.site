9ed7e37e91efd46f1ef72a4750fd654294ebbf59
robinlopez <robin.lopez@4sh.fr>
2016-03-14 20:44:39 +0100

    Init skin sponsors

--- 2016/src/modules/bdxio/angular/components/partners/PartnersListComponent.ts
+++ 2016/src/modules/bdxio/angular/components/partners/PartnersListComponent.ts
@@ -6,13 +6,23 @@ export class PartnersListComponent implements ng.IDirective {
     // Pour une liste complète des attributs possibles sur un attendee, cf SharedModel dans les
     // columnFields de partners
     public template: string = `
-        <div>
-            <div>{{partnersByType.type}}</div>
-            <ul>
-                <li ng-repeat="partner in partnersByType.companies">
-                    {{partner.name}}
-                </li>
-            </ul>
+        <div class="wrapper">
+            <div class="row">
+              <div class="col-sm-12">
+                    <h1 class="section-title text-primary space-top-40 inner-space-left-20 space-bottom-40">{{partnersByType.type}}</h1>
+                </div>
+                <ul class="row">
+                    <li class="item-partner col-xs-12 col-sm-6 col-md-4 col-lg-3" ng-repeat="partner in partnersByType.companies">
+                        <div class="content-partner">
+                            <img src="http://www.esea2015.fr/sites/esea2015.drupal7.aquisante.priv/files/capgemini.png">
+                            <div class="content-infos-partner">
+                                <span class="name-partner">{{partner.name}}</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+              </div>
+            </div>
         </div>
     `
 }
--- 2016/src/modules/bdxio/angular/components/partners/PartnersPageComponent.ts
+++ 2016/src/modules/bdxio/angular/components/partners/PartnersPageComponent.ts
@@ -3,6 +3,28 @@ import {ISharedModel, ITypedCompaniesByType} from "../../../models/int/ISharedMo
 export class PartnersPageComponent implements ng.IDirective {
     public controller: Function = PartnersPageController;
     public template: string = `
+        <section class="partners wrapper">
+            <div class="row">
+                <div class="col-sm-12">
+                     <h2 class="text-white highlight-text-bold">Partenaires</h2>
+                </div>
+
+                <div class="col-xs-12 col-sm-6">
+
+                <h4 class="text-white highlight-text-bold">
+                        Chaque partenaire soutient financièrement la conférence.
+                        Sans ces sociétés, l'organisation de BDX.io n'aurait pas été possible :
+                        le prix de l'entrée aurait coûté plus cher, certains speakers ne pourraient pas être défrayés,
+                        le repas du midi ne pourrait pas être offert...
+                        N'hésitez donc pas à aller consulter le site de ces sociétés pour en savoir davantage sur elles.
+                     </h4>
+                </div>
+
+                <button type="button" class="btn-view-talks btn btn-primary btn-xlg has-icon-left ">
+                    <i class="fa fa-question-circle"></i>Comment devenir partenaire ?
+                </button>
+            </div>
+        </section>
         <partners-list partners-by-type="$ctrl.partnersByType.gold" />
         <partners-list partners-by-type="$ctrl.partnersByType.silver" />
         <partners-list partners-by-type="$ctrl.partnersByType.bronze" />
