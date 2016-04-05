4967e99b95fe119abeffde571d427cb97326841c
fcamblor <frederic.camblor@4sh.fr>
2016-04-05 11:08:08 +0200

    Added angular-morph + test on news item button

--- 2016/src/index.ts
+++ 2016/src/index.ts
@@ -1,6 +1,7 @@
 import "./modules/application/angular/index";
 import "./modules/bdxio/angular/index";
 import "angular";
+import "angular-morph/dist/angular-morph";
 
 // load our default (non specific) css
 import "font-awesome/css/font-awesome.css";
@@ -8,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles/main.scss";
 import "bootstrap/dist/js/bootstrap.js";
 
-angular.module("app", ["app.application", "bdxio.app"]);
+angular.module("app", ["app.application", "bdxio.app", "ngMorph"]);
 angular.bootstrap(document, ["app"], {
     strictDi: true
 });
--- 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
+++ 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
@@ -7,7 +7,7 @@ export class NewsListComponent implements ng.IDirective {
                 <div class="content-title-new">
                    <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du 00.00.2016</span>
                    <h3 class="text-white">{{::newsItem.title}}</h3>
-                   <button class="btn btn-primary btn-lg has-icon-left"><i class="fa fa-eye"></i>Lire l’article</button>
+                   <button ng-morph-modal="$ctrl.settings" class="btn btn-primary btn-lg has-icon-left"><i class="fa fa-eye"></i>Lire l’article</button>
                 </div>
             </li>
         </ul>
@@ -20,6 +20,27 @@ export class NewsListController {
         { title: "Je déclare le CFP...OUVERT !"},
         { title: "Une nouvelle affiche pour votre machine à café"}
     ];
+
+    public settings = {
+        closeEl: '.close',
+        modal: {
+            template: `
+            <div style="width: 400px; height: 400px; background: red;" >
+              <span class="glyphicon glyphicon-remove close"></span>
+              <form>
+                <p> <label> Email </label> <input type="text" /> </p>
+                <p> <label> Password </label> <input type="password" /> </p>
+                <p> <button> Login </button> </p>
+              </form>
+            </div>
+            `,
+            position: {
+                top: '30%',
+                left: '20%'
+            },
+            fade: true
+        }
+    };
 
     constructor() {
     }
