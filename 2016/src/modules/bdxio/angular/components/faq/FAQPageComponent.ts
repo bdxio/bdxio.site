6a066333a840789bae6a77146bbffdc3bcc2faac
robinlopez <robin.lopez@4sh.fr>
2016-04-08 18:37:10 +0200

    Added section FAQ

--- 2016/src/modules/bdxio/angular/components/faq/FAQPageComponent.ts
+++ 2016/src/modules/bdxio/angular/components/faq/FAQPageComponent.ts
@@ -1,10 +1,24 @@
 export class FAQPageComponent implements ng.IDirective {
     public controller: Function = FAQPageController;
     public template: string = `
-    <section class="faq-content wrapper">
+    <section class="faq-gold-content wrapper">
             <div class="row">
-                <div class="col-md-12">
-
+                 <div class="col-sm-12">
+                    <h1 class="section-title text-white space-top-40 space-left-50 space-bottom-40">Gold</h1>
+                </div>
+            </div>
+    </section>
+    <section class="faq-silver-content wrapper">
+            <div class="row">
+                 <div class="col-sm-12">
+                    <h1 class="section-title text-white space-top-40 space-left-50 space-bottom-40">Silver</h1>
+                </div>
+            </div>
+    </section>
+    <section class="faq-bronze-content wrapper">
+            <div class="row">
+                 <div class="col-sm-12">
+                    <h1 class="section-title text-white space-top-40 space-left-50 space-bottom-40">Bronze</h1>
                 </div>
             </div>
     </section>
