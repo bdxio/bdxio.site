70e88a139136d601cd70da3e1e02f1c2c366492b
robinlopez <robin.lopez@4sh.fr>
2016-04-08 18:37:32 +0200

    Fix overflow new list home

--- 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
+++ 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
@@ -3,7 +3,10 @@ export class NewsListComponent implements ng.IDirective {
     public template: string = `
         <ul class="content-list-news">
             <li class="item-new" ng-repeat="newsItem in $ctrl.availableNews">
-                <img src="https://i.ytimg.com/vi/GtAfp55gBeQ/maxresdefault.jpg">
+                <div class="container-img">
+                    <img src="https://i.ytimg.com/vi/GtAfp55gBeQ/maxresdefault.jpg">
+                </div>
+
                 <div class="content-title-new">
                    <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du 00.00.2016</span>
                    <h3 class="text-white">{{::newsItem.title}}</h3>
