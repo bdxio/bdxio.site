a40568362329fdd46635488436758bf85de5c80e
fcamblor <frederic.camblor@4sh.fr>
2016-05-22 03:50:00 +0200

    Allowing to generate morph-modal settings dynamically depending on current news

--- 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
+++ 2016/src/modules/bdxio/angular/components/home/NewsListComponent.ts
@@ -11,14 +11,14 @@ export class NewsListComponent implements ng.IDirective {
                     </div>
     
                     <div class="content-title-new">
-                       <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du 00.00.2016</span>
+                       <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du {{::newsItem.date}}</span>
                        <h3 class="text-white">{{::newsItem.title}}</h3>
-                       <button ng-morph-modal="$ctrl.settings" class="btn btn-primary btn-lg has-icon-left"><i class="fa fa-eye"></i>Lire l’article</button>
+                       <button ng-morph-modal="$ctrl.createMorphModalSettingsFor(newsItem)" class="btn btn-primary btn-lg has-icon-left"><i class="fa fa-eye"></i>Lire l’article</button>
                     </div>
                 </li>
             </carousel>
         </ul>
-    `
+    `;
 }
 export class NewsListController {
 
@@ -26,60 +26,48 @@ export class NewsListController {
 
     public availableNews: Array<INews>;
 
-    public settings = {
-        closeEl: '.close',
-        target: 'body',
-        modal: {
-            template: `
+    constructor(sharedModel: ISharedModel) {
+        sharedModel.dataLoaded.then(() => {
+            this.availableNews = sharedModel.data.news;
+        });
+    }
+
+    public createMorphModalSettingsFor(news: INews) {
+        var settings = {
+            closeEl: '.close',
+            target: 'body',
+            modal: {
+                template: `
             <div class="modal-morph">
               <span class="glyphicon glyphicon-remove close"></span>
               <div class="row">
                 <div class="col-md-12 header-modal">
                     <div class="row">
-                        <h3 class="col-md-8 text-white highlight-text-bold force-inner-space-left-30 title">Je déclare le CFP... OUVERT !</h3>
+                        <h3 class="col-md-8 text-white highlight-text-bold force-inner-space-left-30 title">${news.title}</h3>
                         <div class="col-md-4">
-                            <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du 00.00.2016</span>
+                            <span class="date-new text-white"><i class="fa fa-calendar-o space-right-5"></i> Billet du ${news.date}</span>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-12 content-modal">
                     <div class="col-md-12">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-                        et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                        pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-                        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-                        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-                        Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in,
-                        viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
-                        Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-                        Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero,
-                        sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
-                        hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero
-                        venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt.
-                        Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat,
-                        leo eget bibendum sodales, augue velit cursus nunc,</p><br>
-                        <img src="https://i.ytimg.com/vi/GtAfp55gBeQ/maxresdefault.jpg">
+                        <p>${news.content}</p><br>
+                        <img src="${news.picture}" style="${news.pictureStyles}">
                     </div>
                 </div>
                 <div class="col-md-12 footer-modal">
-                      <button class="btn btn-secondary btn-md has-icon-left float-left"><i class="fa fa-angle-left"></i>Je déclare le CFT...</button>
-                      <button class="btn btn-secondary btn-md has-icon-right float-right">Je déclare le CFT...<i class="fa fa-angle-right"></i></button>
+                      <button class="btn btn-secondary btn-md has-icon-left float-left"><i class="fa fa-angle-left"></i>${news.title}</button>
+                      <button class="btn btn-secondary btn-md has-icon-right float-right">${news.title}<i class="fa fa-angle-right"></i></button>
                 </div>
               </div>
-            </div>
-            `,
-            position: {
-                top: '20%',
-                left: '30%'
-            },
-            fade: true
-        }
-    };
-
-    constructor(sharedModel: ISharedModel) {
-        sharedModel.dataLoaded.then(() => {
-            this.availableNews = sharedModel.data.news;
-        });
+            </div>`,
+                position: {
+                    top: '20%',
+                    left: '30%'
+                },
+                fade: true
+            }
+        };
+        return settings;
     }
 }
