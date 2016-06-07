e75e2c3ccc8f614614e6409e64509db0e6002956
fcamblor <frederic.camblor@4sh.fr>
2016-06-07 23:39:14 +0200

    Hiding speakers attendees section from menu

--- 2016/src/modules/application/angular/components/AppNavMenuComponent.ts
+++ 2016/src/modules/application/angular/components/AppNavMenuComponent.ts
@@ -69,7 +69,7 @@ export class AppNavMenuComponent implements ng.IDirective {
                                     Participants<i class="fa fa-angle-down"></i>
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a ng-click="$ctrl.selectMenu('participant-speakers')">Speakers</a></li>
+                                    <!-- <li><a ng-click="$ctrl.selectMenu('participant-speakers')">Speakers</a></li> -->
                                     <li><a ng-click="$ctrl.selectMenu('participant-orgas')">Organisateurs</a></li>
                                 </ul>
                             </li>
