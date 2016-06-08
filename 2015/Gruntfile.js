fcb57432ba7ea04ca24ff24575663bc1cf12a087
Elian ORIOU <elian.oriou@4sh.fr>
2016-06-08 14:00:11 +0200

    update 2015 gruntfile

--- 2015/Gruntfile.js
+++ 2015/Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         yeoman: {
             // Configurable paths
             app: 'app',
-            dist: 'dist'
+            dist: 'dist/2015'
         },
 
         // Watches files for changes and runs tasks based on the changed files
