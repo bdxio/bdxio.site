e8dcc9d4b5eb01be745be1a832dc166008b5bbe0
robinlopez <robin.lopez@4sh.fr>
2016-06-09 14:28:59 +0200

    Fix breakpoints carousel

--- 2016/src/modules/bdxio/angular/components/carousel/CarouselComponent.ts
+++ 2016/src/modules/bdxio/angular/components/carousel/CarouselComponent.ts
@@ -15,23 +15,23 @@ export class CarouselComponent implements ng.IDirective {
                 arrows: true,
                 dots: true,
                 infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 responsive: [
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
                             infinite: true,
                             dots: true
                         }
                     },
                     {
-                        breakpoint: 600,
+                        breakpoint: 700,
                         settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
+                            slidesToShow: 1,
+                            slidesToScroll: 1
                         }
                     },
                     {
