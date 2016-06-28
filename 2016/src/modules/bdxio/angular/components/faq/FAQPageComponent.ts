e8468974cb045c5f3aff2e50732994c2e7fa81d3
Damien Varron <damien@varron.fr>
2016-06-28 13:01:28 +0200

    Add Code of Conduct

--- 2016/src/modules/bdxio/angular/components/faq/FAQPageComponent.ts
+++ 2016/src/modules/bdxio/angular/components/faq/FAQPageComponent.ts
@@ -4,7 +4,8 @@ export class FAQPageComponent implements ng.IDirective {
     <section class="faq wrapper">
             <div class="row">
                <ul class="col-sm-3 nav nav-pills nav-stacked space-top-25">
-                    <li ng-class="{'active' : activeSection === 'sponsors'}"><a ng-click="activeSection = 'sponsors'" ng-init="activeSection = 'sponsors'">Sponsors</a></li>
+                    <li ng-class="{'active' : activeSection === 'coc'}" ><a ng-click="activeSection = 'coc'" ng-init="activeSection = 'coc'">Code de Conduite</a></li>
+                    <li ng-class="{'active' : activeSection === 'sponsors'}"><a ng-click="activeSection = 'sponsors'">Sponsors</a></li>
                     <li ng-class="{'active' : activeSection === 'speakers'}"><a ng-click="activeSection = 'speakers'">Speakers</a></li>
                     <li ng-class="{'active' : activeSection === 'participants'}"><a ng-click="activeSection = 'participants'">Participants</a></li>
                     <li ng-class="{'active' : activeSection === 'jury'}" ><a ng-click="activeSection = 'jury'">Jury</a></li>
@@ -12,7 +13,78 @@ export class FAQPageComponent implements ng.IDirective {
 
                 <div class="col-sm-9 faq-container-sections">
 
-                    <!-- START FAQ Sponsors !-->
+                    <!-- START FAQ Code de Conduite !-->
+                    <div ng-show="activeSection === 'coc'" class="faq-content section-sponsors" ng-init="questionCoc1 = true">
+                        <h1 class="section-title text-primary">Code de Conduite</h1>
+
+                        <h4 class="space-bottom-20">Toutes les personnes qui participent à la
+                        conférence doivent accepter le code de conduite suivant. L'équipe d'organisation
+                        s'attachera à faire respecter ce code durant l'événement. Nous attendons de
+                        la part de chacune des personnes présentes une coopération pour assurer un environnement
+                        sain pour tout le monde.</h4>
+
+                        <h2 class="space-bottom-20">Besoin d'aide&nbsp;?</h2>
+
+                        <ul>
+                            <li>par email : team@bdx.io</li>
+                            <li>par twitter : <a href="https://twitter.com/bdxio">@bdxio</a></li>
+                            <li>par téléphone : 06 07 50 53 73</li>
+                        </ul>
+
+                        <h4 class="space-bottom-20">Notre conférence se veut une expérience sans harcèlement, quelque soit
+                        votre sexe, votre orientation sexuelle, votre handicap, votre apparence
+                        physique, votre poids, votre origine ou votre religion. Nous ne tolérons aucun
+                        harcèlement des personnes présentes à la conférence, quelque soit sa forme. Les
+                        expressions et les images à connotation sexuelle ne sont pas appropriées lors
+                        de l'événement. Ceci inclut les conférences, les ateliers, les soirées, Twitter
+                        et les autres médias en ligne. Les personnes présentes à la conférence qui violent
+                        ces règles peuvent être sanctionnées, voire exclues de la conférence <em>sans
+                        remboursement</em>, à la discrétion de l'équipe d'organisation de la conférence.</h4>
+
+                        <p>Le harcèlement inclut des commentaires oraux sur le sexe, l'orientation
+                        sexuelle, le handicap, l'apparence physique, le poids, l'origine, la religion,
+                        les images à connotation sexuelle dans des lieux publics, les intimidations
+                        délibérées, la traque, la poursuite, un harcèlement photographique ou vidéo,
+                        une suite d'interruption des conférences et des autres événements, un contact
+                        physique inapproprié et des avances sexuelles non désirées.</p>
+
+                        <p>Les personnes à qui il sera demandé d'arrêter tout comportement de
+                        harcèlement doivent arrêter immédiatement.</p>
+
+                        <p>Les sponsors sont aussi sujet à la politique anti-harcèlement. En
+                        particulier, les sponsors doivent s'abstenir d'utiliser toute image ou support à
+                        connotation sexuelle. Ils ne doivent pas non plus engager d'activités à
+                        connotation sexuelle. L'équipe du stand (y compris les volontaires) ne doivent
+                        pas utiliser de vêtements, uniformes ou costumes à connotation sexuelle. Ils ne
+                        doivent pas non plus créer un environnement sexualisé.</p>
+
+                        <p>Si un participant ou une participante a un comportement de harcèlement, l'équipe d'organisation de la
+                        conférence peut prendre toute action qui lui semble adéquate. Cela va d'un
+                        simple avertissement à l'exclusion de la personne de la conférence sans
+                        remboursement.</p>
+
+                        <p>Si vous vous sentez victime de harcelement, si vous pensez que quelqu'un se fait harceler,
+                        et plus généralement en cas de problème, merci de contacter immédiatement un membre
+                        de l'organisation de l'événement. Les membres sont facilement identifiables à leur
+                        t-shirt aux couleurs de l'événement.</p>
+
+                        <p>Nous attendons des personnes présentes à l'événement qu'elles suivent ces règles dans le bâtiment
+                        des conférences et des ateliers, ainsi que pendant les événements sociaux
+                        relatifs à la conférence.</p>
+
+                        <div>
+                        <p><small><em>Version originale et crédit: <a href="http://2012.jsconf.us/#/about">http://2012.jsconf.us/#/about</a> &amp; <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">The Ada Initiative</a><br>
+
+                        Merci d'aider à la traduction et à l'amélioration de ce texte : <a href="https://github.com/leftlogic/confcodeofconduct.com">http://github.com/leftlogic/confcodeofconduct.com</a><br>
+
+                        Ce texte est sous licence <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported License</a></em></small>
+
+                        </p>
+                        </div>
+                    </div>
+                    <!-- END FAQ Code de Conduite !-->
+ 
+                     <!-- START FAQ Sponsors !-->
                     <div ng-show="activeSection === 'sponsors'" class="faq-content section-sponsors" ng-init="questionSponsors1 = true">
                         <h1 class="section-title text-primary">Sponsors</h1>
                         <h4 class="space-bottom-20">L’association BDX.IO, organisatrice de l’événement,
