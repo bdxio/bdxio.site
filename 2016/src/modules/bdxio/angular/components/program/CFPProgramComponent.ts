6d93c067e1301a68b4a83ad1d02a27dff6e06437
Elian ORIOU <elian.oriou@4sh.fr>
2016-08-18 21:59:05 +0200

    allow to filter presentations by track (attached to the previously selected day)

--- 2016/src/modules/bdxio/angular/components/program/CFPProgramComponent.ts
+++ 2016/src/modules/bdxio/angular/components/program/CFPProgramComponent.ts
@@ -14,22 +14,31 @@ export class CFPProgramComponent implements ng.IDirective {
 
     public template:string = `
         <div class="cfp-program">
-            <div ng-repeat="day in $ctrl.event.days" ng-click="$ctrl.selectCurrentDay(day, $index)" class="day-selector">
-                J {{ $index + 1 }} - {{ day.date | date: $ctrl.datePattern }}
+            <div class="cfp-program-filter">
+                <!--Filtering bar-->
+                <div ng-repeat="day in $ctrl.event.days" ng-click="$ctrl.selectCurrentDay(day, $index)" class="day-selector">
+                    J {{ $index + 1 }} - {{ day.date | date: $ctrl.datePattern }}
+                </div>
+                <div ng-repeat="track in $ctrl.currentDay.tracks" ng-click="$ctrl.filterByTrack(track)" class="track-selector" ng-show="$ctrl.currentDay">
+                    {{ track }}
+                </div>
             </div>
             <div width="100%">
+                <!--Rooms columns-->
                 <div class="row program-header">
                     <div class="column" ng-show="$ctrl.currentDayIndex">J {{ $ctrl.currentDayIndex }}</div>
                     <div class="column" ng-repeat="room in $ctrl.currentDay.rooms">
                         <b>{{ ::room }}</b>
                     </div>
                 </div>
+                <!--Iterate over slots-->
                 <div class="row program-content" ng-repeat="slot in $ctrl.currentDay.slots">
                     <div class="column">
                         <b>{{ ::slot.from | date:$ctrl.timePattern }}</b>
                     </div>
-                    <div class="column" ng-repeat="prez in slot.presentations">
-                        <span ng-show="prez.title">{{ prez.title }} ({{ prez.room }})</span> |
+                    <!--Iterate over all slot's presentations-->
+                    <div class="column" ng-repeat="prez in slot.presentations | filter: $ctrl.filter">
+                        <span ng-show="prez.title">{{ prez.title }}</span> |
                     </div>
                 </div>
             </div>
@@ -43,6 +52,7 @@ export class CFPProgramController {
     private event:ICFPEvent;
     private currentDay:ICFPDay;
     private currentDayIndex:number;
+    private filter:ICFPPresentation = new CFPPresentation();
 
     public datePattern:string = 'dd/MM/yyyy';
     public timePattern:string = 'HH:mm';
@@ -50,5 +60,9 @@ export class CFPProgramController {
     public selectCurrentDay(day:ICFPDay, index:number):void {
         this.currentDay = day;
         this.currentDayIndex = index + 1;
+    }
+
+    public filterByTrack(track:string):void {
+        this.filter.track = track;
     }
 }
