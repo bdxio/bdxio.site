8192ee4ba271a4491ec57641b63508e35e4f5980
Elian ORIOU <elian.oriou@4sh.fr>
2016-08-22 21:27:34 +0200

    improve CFP program component

--- 2016/src/modules/bdxio/angular/components/program/CFPProgramComponent.ts
+++ 2016/src/modules/bdxio/angular/components/program/CFPProgramComponent.ts
@@ -15,28 +15,45 @@ export class CFPProgramComponent implements ng.IDirective {
 
     public template:string = `
         <div class="row cfp-program">
-            <!--<div class="cfp-program-event-title">{{ $ctrl.event.name }}</div>-->
-
             <div class="cfp-program-filter col-sm-12">
+
+                <!--Filtering bar-->
                 <div class="row">
-                    <!--Filtering bar-->
-                    <div class="container-day-selector col-sm-2">
-                        <h4 class="text-primary space-left-10">Jours :</h4>
-                        <div class="col-sm-12" ng-repeat="day in $ctrl.event.days" ng-click="$ctrl.selectCurrentDay(day, $index)" class="day-selector">
-                            J {{ $index + 1 }} - {{ day.date | date: $ctrl.datePattern }}
+
+                    <div class="col-sm-2">
+                        <!--Days-->
+                        <div class="row">
+                            <h4 class="text-primary space-left-10">Jours</h4>
+                            <div class="col-sm-12" ng-repeat="day in $ctrl.event.days" ng-click="$ctrl.selectCurrentDay(day, $index)" class="day-selector">
+                                J {{ $index + 1 }} - {{ day.date | date: $ctrl.datePattern }}
+                            </div>
                         </div>
 
-                        <h4 class="text-primary space-left-10">Salles :</h4>
-                        <div class="place-selector" ng-class="{'select' : true}"></div>
+                        <!--Rooms-->
+                        <div class="row" ng-show="$ctrl.currentDay">
+                            <h4 class="text-primary space-left-10">Salles</h4>
+                            <ui-select ng-model="$ctrl.filter.room" class="col">
+                                <ui-select-match placeholder="Salle...">{{ $select.selected }}</ui-select-match>
+                                <ui-select-choices repeat="room in $ctrl.currentDay.rooms">
+                                    {{ room }}
+                                </ui-select-choices>
+                            </ui-select>
+                        </div>
                     </div>
 
+                    <!--Tracks-->
                     <div class="container-track-selector col-sm-6" ng-show="$ctrl.currentDay">
-                      <h4 class="text-primary space-left-10">Tracks :</h4>
-                      <div ng-repeat="track in $ctrl.currentDay.tracks" ng-click="$ctrl.filterByTrack(track)"
-                           class="track-selector" ng-class="{'select' : true}">
-                           <!-- TODO Add dynamic icon track -->
+                      <h4 class="text-primary space-left-10">Tracks</h4>
+                      <div ng-repeat="track in $ctrl.currentDay.tracks" ng-click="$ctrl.toggleFilteringByTrack(track)"
+                           class="track-selector" ng-class="{'select' : $ctrl.filter.track === track }">
                            <i class="bdx-design"></i>{{ track }}
                        </div>
+                    </div>
+
+                    <!--Actions-->
+                    <div class="col-sm-2">&nbsp;</div>
+                    <div class="col-sm-2" ng-show="$ctrl.currentDay">
+                        <button ng-click="$ctrl.clearFilter()">Reinitialiser</button>
                     </div>
                 </div>
             </div>
@@ -56,6 +73,7 @@ export class CFPProgramComponent implements ng.IDirective {
 
                 <!--Iterate over slots-->
                 <div class="row program-content" ng-repeat="slot in $ctrl.currentDay.slots">
+
                     <div class="col-lg-1 col-xs-3 time">
                         <b><i class="fa fa-clock-o space-right-10"></i>{{ ::slot.from | date:$ctrl.timePattern }}</b>
                     </div>
@@ -63,8 +81,8 @@ export class CFPProgramComponent implements ng.IDirective {
                     <!--Iterate over all slot's presentations-->
                     <div class="col-lg-11 col-xs-9">
                         <div class="row">
-                            <div class="prez large-item  col-xs-12" ng-repeat="prez in slot.presentations | filter: $ctrl.filter">
-                                <span ng-show="prez.title">{{ prez.title }}</span>
+                            <div class="prez large-item  col-xs-12" ng-repeat="prez in slot.presentations">
+                                <span ng-show="prez.title && $ctrl.matchFilter($ctrl.filter, prez)">{{ prez.title }}</span>
                             </div>
                         </div>
                     </div>
@@ -91,7 +109,26 @@ export class CFPProgramController {
         this.filter = new CFPPresentation();
     }
 
-    public filterByTrack(track:string):void {
-        this.filter.track = track;
+    public clearFilter():void {
+        this.filter = new CFPPresentation();
+    }
+
+    public toggleFilteringByTrack(track:string):void {
+        if (track === this.filter.track) {
+            delete this.filter.track;
+        } else {
+            this.filter.track = track;
+        }
+    }
+
+    public matchFilter(filter:ICFPPresentation, prez:ICFPPresentation):boolean {
+        if (_.isEmpty(filter)) return true;
+        return _.chain(filter)
+            .keys()
+            .filter((key) => key.indexOf('$') !== 0)
+            .every((key) => {
+                return prez[key] && filter[key] && prez[key] === filter[key];
+            })
+            .value();
     }
 }
