32929b9cd16abd9ba001a16efabca1d5e80c905a
Elian ORIOU <elian.oriou@4sh.fr>
2017-03-16 15:17:33 +0100

    remove full size team picture

--- 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
+++ 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
@@ -3,7 +3,7 @@ export class WelcomeContentComponent implements ng.IDirective {
     public template: string = `
         <section class="welcome-home wrapper">
             <div class="row">
-                <div class="col-sm-6" data-sr="opacity 0.5 1s, reset">
+                <div class="col-sm-12" data-sr="opacity 0.5 1s, reset">
                      <h2 class="text-white">
                          Eyh ! On vous prépare l'édition 2017 de BDX I/O !
                      </h2>
@@ -21,9 +21,6 @@ export class WelcomeContentComponent implements ng.IDirective {
                         <!--<br/>-->
                         <!--Vous pouvez également visiter la page <a class="link link-white" href="http://lanyrd.com/2016/bdxio/" target="_blank">Lanyrd</a> de l'évènement.-->
                      </h4>
-                </div>
-                <div class="col-sm-6" data-sr="opacity 0.5 1s, reset">
-                        <img src="https://cdn.rawgit.com/bdxio/static/cecd4243/members/_W1A4520.jpg" width="700px"/>
                 </div>
                 <a href="https://www.youtube.com/playlist?list=PLUJzERpatfsXGv1q1kolSgriwVZXRqKVw"
                 class="btn-view-talks btn btn-primary btn-xlg has-icon-left" target="_blank">
