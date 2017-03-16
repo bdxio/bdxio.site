6c5fa852f37d8c7b4ec83ec5d0400240290661d0
robinlopez <robin.lopez@4sh.fr>
2017-03-16 16:18:09 +0100

    Fix size typo home

--- 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
+++ 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
@@ -3,13 +3,14 @@ export class WelcomeContentComponent implements ng.IDirective {
     public template: string = `
         <section class="welcome-home wrapper">
             <div class="row">
-                <div class="col-sm-12" data-sr="opacity 0.5 1s, reset">
-                     <h2 class="text-white">
+                <div class="col-sm-12 col-md-7" data-sr="opacity 0.5 1s, reset">
+                     <h1 class="text-white title-home">
                          Eyh ! On vous prépare l'édition 2017 de BDX I/O !
-                     </h2>
-                     <h4 class="text-white">
+                     </h1>
+                     <h3 class="text-white">
                         <b>Merci !</b>
-                        Un grand merci à tous de votre participation à l'édition 2016 de bdx.io ! 
+                         Un grand merci à tous de votre participation à l'édition 2016 de bdx.io ! 
+                        <br/>
                         <br/>
                         Lors de cette édition, vous avez été très nombreux à nous soutenir et à nous encourager, et on compte bien remettre ça en 2017 ! 
                         <br/>
@@ -20,7 +21,7 @@ export class WelcomeContentComponent implements ng.IDirective {
                         <!--<br/>-->
                         <!--<br/>-->
                         <!--Vous pouvez également visiter la page <a class="link link-white" href="http://lanyrd.com/2016/bdxio/" target="_blank">Lanyrd</a> de l'évènement.-->
-                     </h4>
+                     </h3>
                 </div>
                 <a href="https://www.youtube.com/playlist?list=PLUJzERpatfsXGv1q1kolSgriwVZXRqKVw"
                 class="btn-view-talks btn btn-primary btn-xlg has-icon-left" target="_blank">
