636d29098103b640309a6e729baa3f15a14c11a1
Elian ORIOU <elian.oriou@4sh.fr>
2017-03-16 10:08:32 +0100

    add team picture on home page

--- 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
+++ 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
@@ -3,26 +3,28 @@ export class WelcomeContentComponent implements ng.IDirective {
     public template: string = `
         <section class="welcome-home wrapper">
             <div class="row">
-                <div class="col-sm-12" data-sr="opacity 0.5 1s, reset">
+                <div class="col-sm-6" data-sr="opacity 0.5 1s, reset">
                      <h2 class="text-white">
                          Eyh ! On vous prépare l'édition 2017 de BDX I/O !
                      </h2>
                      <h4 class="text-white">
                         <b>Merci !</b>
                         Un grand merci à tous de votre participation à l'édition 2016 de bdx.io ! 
-                        <br>
+                        <br/>
                         Lors de cette édition, vous avez été très nombreux à nous soutenir et à nous encourager, et on compte bien remettre ça en 2017 ! 
-                        <br>
+                        <br/>
                         A très vite !
-                        <br>
-                        <!--Le programme de l'évènement est disponible sur l'application<br>-->
+                        <br/><br/>
+                        <!--Le programme de l'évènement est disponible sur l'application<br/>-->
                         <!--<a class="link link-white" href="http://www.voxxr.in/" target="_blank">Voxxrin</a> (en web, sur android, et sur iPhone).-->
-                        <!--<br>-->
-                        <!--<br>-->
+                        <!--<br/>-->
+                        <!--<br/>-->
                         <!--Vous pouvez également visiter la page <a class="link link-white" href="http://lanyrd.com/2016/bdxio/" target="_blank">Lanyrd</a> de l'évènement.-->
                      </h4>
                 </div>
-
+                <div class="col-sm-6" data-sr="opacity 0.5 1s, reset">
+                        <img src="https://cdn.rawgit.com/bdxio/static/cecd4243/members/_W1A4520.jpg" width="700px"/>
+                </div>
                 <a href="https://www.youtube.com/playlist?list=PLUJzERpatfsXGv1q1kolSgriwVZXRqKVw"
                 class="btn-view-talks btn btn-primary btn-xlg has-icon-left" target="_blank">
                      <i class="fa fa-play"></i>Vidéos des talks 2016
