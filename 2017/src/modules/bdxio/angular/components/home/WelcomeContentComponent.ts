bc7aa4477104ffbe62b0020e64df1fdd7d7d5134
Elian ORIOU <elian.oriou@4sh.fr>
2017-11-14 12:59:42 +0100

    update welcome message

--- 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
+++ 2017/src/modules/bdxio/angular/components/home/WelcomeContentComponent.ts
@@ -5,24 +5,15 @@ export class WelcomeContentComponent implements ng.IDirective {
             <div class="row">
                 <div class="col-sm-12 col-md-7" data-sr="opacity 0.5 1s, reset">
                      <h1 class="text-white title-home">
-                         Eyh ! On vous prépare l'édition 2017 de BDX I/O !
+                         Merci à tous de votre participation à BDX I/O 2017 !
                      </h1>
                      <h3 class="text-white">
-                        <!--<b>Merci !</b>-->
-                         <!--Un grand merci à tous de votre participation à l'édition 2016 de bdx.io !-->
-                        <!--<br/>-->
-                        <br/>
-                        La prochaine édition se déroulera le <strong>10 Novembre 2017 à l'ENSEIRB-MATMECA</strong> : save the date !
-                        <br/>
-                        <br/>
-                        La billetterie est maintenant fermée, merci à vous tous pour votre confiance ! On vous prépare un programme qui, 
-                        nous l'esperons, vous plaira ! D'autre part, le CFP est maintenant fermé, merci à tous les orateurs pour 
-                        leurs propositions !
-                        <br/>
-                        <br/>
-                        A bientôt !
-                        <!--Le programme de l'évènement est disponible sur l'application<br/>-->
-                        <!--<a class="link link-white" href="http://www.voxxr.in/" target="_blank">Voxxrin</a> (en web, sur android, et sur iPhone).-->
+                        Le programme de l'évènement est disponible <a class="link link-white" href="https://bdx.io/#/program" target="_blank">ici !</a>
+                        <br>
+                        Les vidéos 2017 sont disponibles <a class="link link-white" href="https://www.youtube.com/playlist?list=PLUJzERpatfsW5PbUcMAIOIqfCoqS7K1F1" target="_blank">ici !</a>
+                        <br>
+                        <br>
+                        On va essayer de dormir un peu :) et on vous dit à l'année prochaine ! 
                         <!--<br/>-->
                         <!--<br/>-->
                         <!--Vous pouvez également visiter la page <a class="link link-white" href="http://lanyrd.com/2016/bdxio/" target="_blank">Lanyrd</a> de l'évènement.-->
