82481db202ee7186b35f707c480e84b88c5114f4
jbehuet <jbehuet@gmail.com>
2018-03-18 18:45:03 +0100

    Display all news on news page

--- src/app/pages/News.page.js
+++ src/app/pages/News.page.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import displayPage          from './_PageContainer';
+import displayPage from './_PageContainer';
 
 class News extends Component {
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
     }
 
     render() {
+        const { News } = this.props.gsheet;
+        const newsDisplayed = News.filter(n => n['Publiée'] == '1');
+
         return (
             <div>
                 <h1>News</h1>
+                {newsDisplayed.map((news, i) => {
+                    return (
+                        <div key={`'news_${i}`}>
+                            <h2>{news.Titre}</h2>
+                            <h4>Le {news.Date}</h4>
+                            <img src={news.Thumbnail} />
+                            <div dangerouslySetInnerHTML={{ __html: news.Contenu }}></div>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
