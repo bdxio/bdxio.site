f72a1830bc5cfdbcbd750ce5a72e3c1bc3353eec
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-03-22 20:06:58 +0100

    Edit align element and background color

--- src/app/components/MetricsBar.component.js
+++ src/app/components/MetricsBar.component.js
@@ -8,13 +8,29 @@ class MetricsBar extends Component {
 
     render() {
         return (
-            <div>
-                <div><Link to="/gallery">+</Link></div>
-                <div>500 PARTICIPANTS</div>
-                <div>55 TALKS</div>
-                <div>52 SPEAKERS</div>
-                <div><Link to="/videos">Play</Link></div>
+            <div className="columns align-middle metricsBar">
+                <div className="row">
+                    <div className="columns small-6 align-center align-middle metricsBar-content"><Link to="/gallery">+</Link></div>
+
+                    <div className="columns auto align-center align-middle metricsBar-content">
+                        <div className="row">
+                            <h4 className="columns metricsBar-content-number">500</h4>
+                            <div className="columns">PARTICIPANTS</div>
+                        </div>
+                        <div className="row">
+                            <h4 className="columns metricsBar-content-number">52</h4>
+                            <div className="columns">TALKS</div>
+                        </div>
+                        <div className="row">
+                            <h4 className="columns metricsBar-content-number">52</h4>
+                            <div className="columns">SPEAKERS</div>
+                        </div>
+                    </div>
+
+                    <div className="columns small-6 align-center align-middle metricsBar-content"><Link to="/videos">Play</Link></div>
+                </div>
             </div>
+
         );
     }
 }
