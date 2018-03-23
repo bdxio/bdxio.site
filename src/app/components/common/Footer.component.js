ca0a55060bb02a4b2a1aaa20e09400a55b9fbaef
cDaffe <celine.daffe@4sh.fr>
2018-03-23 14:30:12 +0100

    Add Footer content

--- src/app/components/common/Footer.component.js
+++ src/app/components/common/Footer.component.js
@@ -9,6 +9,21 @@ class Footer extends Component {
     render() {
         return (
             <div>
+                <div>
+                    <div>Vous souhaitez être tenu•e informé•e ?<br/>Inscrivez-vous à la newsletter</div>
+                    <div><input type="text" placeholder={'Email'}/></div>
+                    <div><button type={'button'} className={'button'}>S'inscrire</button></div>
+                </div>
+                <div>
+                    <ul>
+                        <li>Accueil</li>
+                        <li>Privacy</li>
+                        <li>Termes et conditions</li>
+                        <li>A propos</li>
+                        <li>Contact</li>
+                    </ul>
+                    <div>twitter facebook linkedin</div>
+                </div>
                 @2018 - BDX I/O
             </div>
         );
