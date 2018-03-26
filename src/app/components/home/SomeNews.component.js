75c88655050ad858d19b34ecd7ef8a229e142727
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-03-26 21:22:33 +0200

    Fix img element

---
+++ src/app/components/home/SomeNews.component.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class SomeNews extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0,
+            news: []
+        }
+    }
+
+    goToCarouselPage(index) {
+        this.setState({ index })
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            news: nextProps.news.filter(n => n['Publiée'] == '1')
+        });
+    }
+
+    render() {
+        const { news, index } = this.state
+        const newsDisplayed = news.slice(3 * index, 3 + 3 * index);
+        const nbSelectors = Math.ceil(news.length / 3);
+        return (
+            <div className="row someNews">
+                <div className="columns text-center">
+                    <h5 className="sectionTitleBg">Nos Actualités en bref</h5>
+                    <h6 className="sectionSubtitleBg">Que se passe-t-il avec BDX i/o ?</h6>
+
+                    <div className="row align-center someNews-content">
+                        {newsDisplayed.map((n, i) => {
+                            return (
+                                <div className="columns small-3 someNews-content-item">
+                                    <div className="row text-left" key={`'news_'${i}`}>
+                                        <img src={n.Thumbnail} />
+                                        <div className="columns shrink someNews-content-item-title">{n.Titre}</div>
+                                        <div dangerouslySetInnerHTML={{ __html: n.Contenu }}></div>
+                                        <div><Link to={'/news'}>Lire l'article ></Link></div>
+                                    </div>
+                                </div>
+
+                            );
+                        })}
+                    </div>
+
+                    <div>
+                        {[...Array(nbSelectors)].map((v, i) => {
+                            return (
+                                <button key={`'select_'${i}`} type="button" onClick={() => { this.goToCarouselPage(i) }}>•</button>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <Link to={"/news"}>Les voir tous</Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+SomeNews.propTypes = {
+    news: PropTypes.array.isRequired,
+}
+
+export default SomeNews
