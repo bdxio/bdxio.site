642d47400ec645e9a130d95f4170e16eeac32758
cDaffe <celine.daffe@4sh.fr>
2018-03-26 11:43:30 +0200

    Add timeRemaining calculation

--- src/app/components/Countdown.component.js
+++ src/app/components/Countdown.component.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import Moment from 'moment';
 
 class Countdown extends Component {
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
+        const now = Moment.now();
+        const later =  Moment([2018, 11, 9, 8]);
+        this.state = {
+            timeRemaining:  Moment.duration(later.diff(now))
+        };
     }
 
     render() {
+        const { timeRemaining } = this.state;
         return (
-            <div>
-                <span>211 jours</span>
-                <span>18 heures</span>
-                <span>27 minutes</span>
-                <span>34 secondes</span>
+            <div className={'text-center'}>
+                <div>{timeRemaining._data.months} mois {timeRemaining._data.days} jours {timeRemaining._data.hours} heures {timeRemaining._data.minutes} minutes</div>
             </div>
         );
     }
