be20652aea496afb5ec68010816a7935345fc702
jbehuet <jbehuet@gmail.com>
2018-03-31 00:43:07 +0200

    Edit Quote add to constants

--- src/app/components/home/Quote.component.js
+++ src/app/components/home/Quote.component.js
@@ -1,27 +1,5 @@
 import React, { Component } from 'react';
-
-const QUOTES = [
-    {
-        text: '"Each of us is a unique strand in the intricate web of life and here to make a contribution."',
-        author: 'Deepak Chopra',
-    },
-    {
-        text: '"Le fossé séparant théorie et pratique est moins large en théorie qu’il ne l’est en pratique."',
-        author: ''
-    },
-    {
-        text: '"The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past."',
-        author: 'Tim Berners-Lee'
-    },
-    {
-        text: '"On the Web we all become small-town visitors lost in the big city."',
-        author: 'Alison Gopnik'
-    },
-    {
-        text: '"Everyday life is like programming, I guess. If you love something you can put beauty into it."',
-        author: 'Donald Knuth'
-    },
-]
+import { Constant } from '../../constants';
 
 class Quote extends Component {
     constructor(props) {
@@ -29,8 +7,8 @@ class Quote extends Component {
     }
 
     render() {
-        const rnd = Math.floor(Math.random() * QUOTES.length);
-        const quote = QUOTES[rnd];
+        const rnd = Math.floor(Math.random() * Constant.quotes.length);
+        const quote = Constant.quotes[rnd];
 
         return (
             <div className="row align-middle home-team">
--- src/app/constants.js
+++ src/app/constants.js
@@ -3,4 +3,26 @@ import Moment from 'moment';
 export const Constant = {
     gSheetKey: '1bXJCYX-b4cyDW4sT2xWTx5rYL6Mb3LmBTMthge_J6KA',
     eventDate: Moment([2018, 11, 9, 8]),
+    quotes: [
+        {
+            text: '"Each of us is a unique strand in the intricate web of life and here to make a contribution."',
+            author: 'Deepak Chopra',
+        },
+        {
+            text: '"Le fossé séparant théorie et pratique est moins large en théorie qu’il ne l’est en pratique."',
+            author: ''
+        },
+        {
+            text: '"The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past."',
+            author: 'Tim Berners-Lee'
+        },
+        {
+            text: '"On the Web we all become small-town visitors lost in the big city."',
+            author: 'Alison Gopnik'
+        },
+        {
+            text: '"Everyday life is like programming, I guess. If you love something you can put beauty into it."',
+            author: 'Donald Knuth'
+        },
+    ]
 }
