64189ac1c81c5f64bfacf44dab01f0b7d761f42b
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-02 19:10:19 +0200

    skin component size/ position

--- src/app/components/common/Footer.component.js
+++ src/app/components/common/Footer.component.js
@@ -8,24 +8,28 @@ class Footer extends Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <div>Vous souhaitez être tenu•e informé•e ?<br/>Inscrivez-vous à la newsletter</div>
-                    <div><input type="text" placeholder={'Email'}/></div>
-                    <div><button type={'button'} className={'button'}>S'inscrire</button></div>
+            <div className="row footer">
+                <div className="columns align-center align-middle footer-signIn">
+                    <div>Vous souhaitez être tenu(e) informé(e) ?<br />Inscrivez-vous à la newsletter</div>
+                    <input className="small-2" type="text" placeholder={'Email'} />
+                    <div><button type={'button'} className={'button small white'}>S'inscrire</button></div>
                 </div>
-                <div>
-                    <ul>
-                        <li>Accueil</li>
-                        <li>Privacy</li>
-                        <li>Termes et conditions</li>
-                        <li>A propos</li>
-                        <li>Contact</li>
+
+                <div className="columns footer-item">
+                    <ul className="row">
+                        <li className="columns shrink">Accueil</li>
+                        <li className="columns shrink">Privacy</li>
+                        <li className="columns shrink">Termes et conditions</li>
+                        <li className="columns shrink">A propos</li>
+                        <li className="columns shrink">Contact</li>
+                        <li className="columns auto text-right">twitter</li>
+                        <li className="columns shrink text-right">facebook</li>
+                        <li className="columns shrink text-right">linkedin</li>
                     </ul>
-                    <div>twitter facebook linkedin</div>
                 </div>
-                @2018 - BDX I/O
-            </div>
+
+                <small className="columns footer-date">@2018 - BDX I / O</small>
+            </div >
         );
     }
 }
