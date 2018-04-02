312f59ecce2a644ac57221db8a55ac5f5dd9bf5f
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-02 15:05:46 +0200

    Fix size container

--- src/app/components/home/MetricsBar.component.js
+++ src/app/components/home/MetricsBar.component.js
@@ -9,36 +9,36 @@ class MetricsBar extends Component {
     render() {
         return (
             <div className="row  align-middle metricsBar">
-                <div className="columns small-2 align-center metricsBar-picture">
+                <div className="columns small-2 text-center metricsBar-picture">
                     <Link to="/gallery">+</Link>
                 </div>
 
                 <div className="columns auto">
                     <div className="row metricsBar-content text-center">
-                        <div className="columns small-2 ">
+                        <div className="columns">
                             <h3 className="metricsBar-content-number">46</h3>
                             <div className="">TALKS</div>
                         </div>
-                        <div className="columns small-2 seperateElement">
+                        <div className="columns seperateElement">
                             <h3 className="metricsBar-content-number">~50</h3>
                             <div className="">SPEAKERS</div>
                         </div>
-                        <div className="columns small-3 seperateElement">
+                        <div className="columns seperateElement">
                             <h3 className="metricsBar-content-number">500</h3>
                             <div className="">PARTICIPANTS</div>
                         </div>
-                        <div className="columns small-2 seperateElement">
+                        <div className="columns seperateElement">
                             <h3 className="metricsBar-content-number">1</h3>
                             <div className="">Hackerspace</div>
                         </div>
-                        <div className="columns small-2">
+                        <div className="columns">
                             <h3 className="metricsBar-content-number">3</h3>
                             <div className="">Hands'on</div>
                         </div>
                     </div>
                 </div>
 
-                <div className="columns small-2 align-center metricsBar-video">
+                <div className="columns small-2 text-center metricsBar-video">
                     <a herf="https://www.youtube.com/channel/UCA7pEYY0BlgCdpbnjhCDezQ">Play</a>
                 </div>
             </div>
