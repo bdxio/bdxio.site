ee3416bfaf5c5f5adb5b65bdeb7adfb19638e369
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-02 15:06:22 +0200

    Fix size container

--- src/app/components/home/SomeSpeakersPresentation.component.js
+++ src/app/components/home/SomeSpeakersPresentation.component.js
@@ -12,33 +12,29 @@ class SomeSpeakersPresentation extends Component {
                 <div className="column small-24 sectionTitle">
                     <h5 className="sectionTitle">Nos speakers</h5>
                     <h6 className="sectionSubTitle">On vous a choisi quelques profils de façon aléatoire de façon à vous mettre l'eau à la bouche</h6>
-
-                    <div className="row small-4 speakers-container">
-                        <div className="column speakers-container-content">
-                            <div>photo</div>
-                            <div className="column speakers-container-content">
-                                <div>text</div>
-                            </div>
-                            <div>
-                                <div>photo</div>
-                                <div>text</div>
-                            </div>
-                            <div>
-                                <div>text</div>
-                                <div>photo</div>
-                            </div>
-                            <div>
-                                <div>text</div>
-                                <div>photo</div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
-                <div className="column align-center">
-                        <Link className="columns shrink text-center button small secondary" to={"/speakers"}>Les voir tous</Link>
+                <div className="column small-24 speakers-container">
+                    <div className="row speakers-container-content">
+                        <div className="column auto align-center speakers-container-content-picture"></div>
+                        <div className="column auto align-center speakers-container-content-text">text</div>
+
+                        <div className="column auto align-center speakers-container-content-picture"></div>
+                        <div className="column auto align-center speakers-container-content-text">text</div>
                     </div>
-            </div>
+
+                    <div className="row small-24 speakers-container-content">
+                        <div className="column auto align-center speakers-container-content-text">text</div>
+                        <div className="column auto align-center speakers-container-content-picture">photo</div>
+
+                        <div className="column auto align-center speakers-container-content-text">text</div>
+                        <div className="column auto align-center speakers-container-content-picture">photo</div>
+                    </div>
+                </div>
+                <div className="column align-center">
+                    <Link className="columns shrink text-center button small secondary" to={"/speakers"}>Les voir tous</Link>
+                </div>
+            </div >
         );
     }
 }
