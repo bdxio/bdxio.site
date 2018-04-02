572693a47850940df912369ebf3630c995ca6be5
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-02 16:32:33 +0200

    Fix  size / color / position element

--- src/app/components/home/TicketingAccess.component.js
+++ src/app/components/home/TicketingAccess.component.js
@@ -16,7 +16,10 @@ class TicketingAccess extends Component {
                 <div className="columns text-center">
                     <div className="row align-center ticketingAccess-container">
                         <div className="columns small-3 ticketingAccess-container-item">
-                            <div className="ticketingAccess-container-item-title">32 EUROS / Early Bird</div>
+                            <div className="ticketingAccess-container-item-title">
+                                <div className="ticketingAccess-container-item-title-euros">32,00 €</div>
+                                <div className="ticketingAccess-container-item-title-classe">Early Bird</div>
+                            </div>
                             <div className="ticketingAccess-container-item-status">Individuel</div>
                             <div className="ticketingAccess-container-item-content">
                                 <div>Accès a partir de 8H30 au <span>petit déjeuner</span></div>
@@ -27,7 +30,10 @@ class TicketingAccess extends Component {
                         </div>
 
                         <div className="columns small-3 ticketingAccess-container-item">
-                            <div className="ticketingAccess-container-item-title">64 EUROS / Golden Ticket</div>
+                            <div className="ticketingAccess-container-item-title">
+                                <div className="ticketingAccess-container-item-title-euros">64,00 €</div>
+                                <div className="ticketingAccess-container-item-title-classe">Golden Ticket</div>
+                            </div>
                             <div className="ticketingAccess-container-item-status">Individuel</div>
                             <div className="ticketingAccess-container-item-content">
                                 <div>Donner votre <span>avis</span> concernant les talks</div>
@@ -40,7 +46,10 @@ class TicketingAccess extends Component {
                         </div>
 
                         <div className="columns small-3 ticketingAccess-container-item">
-                            <div className="ticketingAccess-container-item-title">64 EUROS / Classic</div>
+                            <div className="ticketingAccess-container-item-title">
+                                <div className="ticketingAccess-container-item-title-euros">64,00 €</div>
+                                <div className="ticketingAccess-container-item-title-classe">Classic</div>
+                            </div>
                             <div className="ticketingAccess-container-item-status">Individuel</div>
                             <div className="ticketingAccess-container-item-content">
                                 <div>Accès a partir de 8H30 au <span>petit déjeuner</span></div>
