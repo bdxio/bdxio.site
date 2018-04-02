4220d55ac31bb9f11478b03c086dab6f09d6accb
jbehuet <jbehuet@gmail.com>
2018-04-02 18:19:46 +0200

    Random speakers refacto

--- src/app/components/home/SomeSpeakersPresentation.component.js
+++ src/app/components/home/SomeSpeakersPresentation.component.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import PropTypes            from 'prop-types';
-import { Link }             from 'react-router-dom';
-import SomeNews             from './SomeNews.component';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import SomeNews from './SomeNews.component';
+
+const COUNT_DISPLAYED_SPEAKERS = 4;
 
 class SomeSpeakersPresentation extends Component {
     constructor(props) {
         super(props);
-        this.get4Speakers = this.get4Speakers.bind(this);
+        this.getRandomSpeaker = this.getRandomSpeaker.bind(this);
+        this.getRandomSpeakers = this.getRandomSpeakers.bind(this);
     }
 
-    /* TODO add fetch to get 4 speakers randomly */
-    get4Speakers() {
-        if (this.props.speakers.length === 0) {return [];}
-        const speakers4 = [];
-        for(let i = 0; i < 4; i++) {
-            speakers4.push(this.props.speakers[Math.floor(Math.random()*this.props.speakers.length)]);
+    getRandomSpeaker(ref = []) {
+        const { speakers } = this.props;
+        const randomSpeaker = speakers[Math.floor(Math.random() * speakers.length)];
+        const isSelected = ref.find(r => r['Nom'] === randomSpeaker['Nom']) > -1;
+
+        return isSelected ? this.getRandomSpeaker(ref) : randomSpeaker;
+    }
+
+    getRandomSpeakers() {
+        const speakers = [];
+        if (this.props.speakers.length > 0) {
+            for (let i = 0; i < COUNT_DISPLAYED_SPEAKERS; i++) {
+                speakers.push(this.getRandomSpeaker(speakers));
+            }
         }
-        return speakers4;
+        return speakers;
     }
 
     render() {
@@ -29,28 +40,28 @@ class SomeSpeakersPresentation extends Component {
 
                 <div className="column small-24 speakers-container">
                     <div className="row speakers-container-content">
-                        {this.get4Speakers().map((speaker, i) => {
-                         return ( <div key={`'speaker${i}`}>
-                            <img className="column auto align-center speakers-container-content-picture"
-                                 src={speaker['Avatar url']} />
-                            <div className="column auto align-center speakers-container-content-text">
-                                <h4>{speaker['Prénom']} {speaker.Nom}</h4>
-                                <h6>{speaker.Bio}</h6>
-                                <div>
-                                    <span>{speaker.Twitter}</span>
-                                    <span>{speaker.Linkedin}</span>
-                                    <span>{speaker['Google+']}</span>
-                                    <span>{speaker.Blog}</span>
+                        {this.getRandomSpeakers().map((speaker, i) => {
+                            return (<div key={`'speaker_${i}`}>
+                                <img className="column auto align-center speakers-container-content-picture"
+                                    src={speaker['Avatar url']} />
+                                <div className="column auto align-center speakers-container-content-text">
+                                    <h4>{speaker['Prénom']} {speaker['Nom']}</h4>
+                                    <h6>{speaker['Bio']}</h6>
+                                    <div>
+                                        <span>{speaker['Twitter']}</span>
+                                        <span>{speaker['Linkedin']}</span>
+                                        <span>{speaker['Google+']}</span>
+                                        <span>{speaker['Blog']}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                         )
+                            )
                         })}
                     </div>
                 </div>
 
                 <div className="column align-center">
-                        <Link className="columns shrink text-center button small secondary" to={"/speakers"}>Les voir tous</Link>
+                    <Link className="columns shrink text-center button small secondary" to={"/speakers"}>Les voir tous</Link>
                 </div>
             </div>
         );
