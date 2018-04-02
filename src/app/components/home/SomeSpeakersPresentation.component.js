3527291eb57ac0ab15d7bdb5dc51049a888831b5
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-03 01:53:46 +0200

    Fix position/icons

--- src/app/components/home/SomeSpeakersPresentation.component.js
+++ src/app/components/home/SomeSpeakersPresentation.component.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import SomeNews from './SomeNews.component';
 
-const COUNT_DISPLAYED_SPEAKERS = 4;
+const COUNT_DISPLAYED_SPEAKERS = 2;
 
 class SomeSpeakersPresentation extends Component {
     constructor(props) {
@@ -39,19 +39,20 @@ class SomeSpeakersPresentation extends Component {
                 </div>
 
                 <div className="column small-24 speakers-container">
-                    <div className="row speakers-container-content">
+                    <div className="row align-center speakers-container-content">
                         {this.getRandomSpeakers().map((speaker, i) => {
-                            return (<div key={`'speaker_${i}`}>
-                                <img className="column auto align-center speakers-container-content-picture"
-                                    src={speaker['Avatar url']} />
-                                <div className="column auto align-center speakers-container-content-text">
-                                    <h4>{speaker['Prénom']} {speaker['Nom']}</h4>
-                                    <h6>{speaker['Bio']}</h6>
+                            return (<div className="column small-4 align-center speakers-container-content-item" key={`'speaker_${i}`}>
+                                <div className="column auto align-center speakers-container-content-item-picture">
+                                    <img src={speaker['Avatar url']} />
+                                </div>
+                                <div className="column auto align-center speakers-container-content-item-text">
+                                    <h6>{speaker['Prénom']} {speaker['Nom']}</h6>
+                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio']}</div>
                                     <div>
-                                        <span>{speaker['Twitter']}</span>
-                                        <span>{speaker['Linkedin']}</span>
-                                        <span>{speaker['Google+']}</span>
-                                        <span>{speaker['Blog']}</span>
+                                        <span>{speaker['Twitter']}<i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></span>
+                                        <span>{speaker['Linkedin']}<i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></span>
+                                        <span>{speaker['Google+']}<i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></span>
+                                        <span>{speaker['Blog']}<i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></span>
                                     </div>
                                 </div>
                             </div>
@@ -60,7 +61,29 @@ class SomeSpeakersPresentation extends Component {
                     </div>
                 </div>
 
-                <div className="column align-center">
+                <div className="column small-24 speakers-container">
+                    <div className="row align-center speakers-container-content">
+                        {this.getRandomSpeakers().map((speaker, i) => {
+                            return (<div className="column small-4 align-center speakers-container-content-item" key={`'speaker_${i}`}>
+                                <div className="column auto align-center speakers-container-content-item-text">
+                                    <h6>{speaker['Prénom']} {speaker['Nom']}</h6>
+                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio']}</div>
+                                    <div>
+                                        <span>{speaker['Twitter']}</span>
+                                        <span>{speaker['Linkedin']}</span>
+                                        <span>{speaker['Google+']}</span>
+                                        <span>{speaker['Blog']}</span>
+                                    </div>
+                                </div>
+                                <img className="column auto align-center speakers-container-content-item-picture"
+                                    src={speaker['Avatar url']} />
+                            </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className="column align-center speakers-button">
                     <Link className="columns shrink text-center button small secondary" to={"/speakers"}>Les voir tous</Link>
                 </div>
             </div>
