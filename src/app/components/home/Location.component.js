9117faad0c4490f2ae53a39fc0734c61b0ac0540
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-02 19:10:56 +0200

    Skin component color/position/size

--- src/app/components/home/Location.component.js
+++ src/app/components/home/Location.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import IframeContainer      from '../common/IframeContainer.component';
+import IframeContainer from '../common/IframeContainer.component';
 
 class Location extends Component {
     constructor() {
@@ -8,33 +8,35 @@ class Location extends Component {
 
     render() {
         return (
-            <div className="row">
-
-                <div className="columns medium-12">
-                    <div>A 10 minute de <br/>Bordeaux</div>
-                    <div>6 amphis en parallèle <br/>> 50 conférences</div>
-                    <div>15 communautés <br/>> 5àà Développeur•se•s</div>
+            <div className="row location">
+                <div className="columns small-12 align-center location-infos">
+                    <div className="small-3 location-infos-place">A 10 minute de <br />Bordeaux</div>
+                    <div className="small-3 location-infos-amphis">6 amphis en parallèle <br />> 50 conférences</div>
+                    <div className="small-3 location-infos-dev">15 communautés <br />> 500 Développeur(se)s</div>
                 </div>
 
-                <div className="columns medium-12">
+                <div className="columns small-12 location-site">
                     <div className="row">
-                        <div className="columns medium-6">
+                        <div className="columns shrink location-site-place">
                             <h4>ENSEIRB</h4>
                             <div>
-                                1 Avenue de docteur Albert Schweitzer <br/>
+                                1 Avenue de docteur Albert Schweitzer <br />
                                 33400 Talence
                             </div>
+                            <br />
                             <div>
-                                Pour nous rejoindre en transport en commun,<br/>
+                                Pour nous rejoindre en transport en commun,<br />
                                 choisisser le Bus ou le Tram
                             </div>
+                            <br />
                             <div>
                                 Si vous venez en voiture, le parking de l'uiversité est ouvert et gratuit
                             </div>
                         </div>
-                        <div className="columns medium-6">
+
+                         <div className="columns location-site-map">
                             <IframeContainer src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJv4tyl4HYVA0RLvkdcxUEhOw&key=AIzaSyBQYTDJ5joswWG8lQ-Wv9MGYUqHhD9LfCc"
-                                    height="450" width="600" allowFullScreen/>
+                                height="220" width="820" allowFullScreen />
                         </div>
                     </div>
                 </div>
