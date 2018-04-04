c2b9bf7597a588c421b9fa6d2a46a6b816a30fb7
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-04 23:27:30 +0200

    edit bg image

--- src/app/pages/Home.page.js
+++ src/app/pages/Home.page.js
@@ -43,7 +43,7 @@ class Home extends Component {
                     </div>
                 </div>
 
-                <div className="home-headerBackground"><img className="" src="img/svg/header_form.svg"/></div>
+                <div className="home-headerBackground"></div>
 
                 <div className="row text-center home-about">
                     <h5 className="sectionTitle align-center">À propos de Bdxio</h5>
