64690cf8d652f3f705bac80aa6b1107339790e00
jbehuet <jbehuet@gmail.com>
2018-04-05 11:22:50 +0200

    Review random speakers DOM and limit bio

--- src/app/components/home/SomeSpeakersPresentation.component.js
+++ src/app/components/home/SomeSpeakersPresentation.component.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import SomeNews from './SomeNews.component';
 
-const COUNT_DISPLAYED_SPEAKERS = 2;
+const COUNT_DISPLAYED_SPEAKERS = 4;
 
 class SomeSpeakersPresentation extends Component {
     constructor(props) {
@@ -12,18 +12,19 @@ class SomeSpeakersPresentation extends Component {
         this.getRandomSpeakers = this.getRandomSpeakers.bind(this);
     }
 
-    getRandomSpeaker(ref = []) {
+    getRandomSpeaker(ref) {
         const { speakers } = this.props;
         const randomSpeaker = speakers[Math.floor(Math.random() * speakers.length)];
-        const isSelected = ref.find(r => r['Nom'] === randomSpeaker['Nom']) > -1;
+        const isSelected = (ref.findIndex(r => r['Nom'] === randomSpeaker['Nom']) > -1);
 
-        return isSelected ? this.getRandomSpeaker(ref) : randomSpeaker;
+        return (isSelected || (randomSpeaker['Nom'] === "")) ? this.getRandomSpeaker(ref) : randomSpeaker;
     }
 
     getRandomSpeakers() {
         const speakers = [];
         if (this.props.speakers.length > 0) {
-            for (let i = 0; i < COUNT_DISPLAYED_SPEAKERS; i++) {
+            const LIMIT = this.props.speakers.length >= COUNT_DISPLAYED_SPEAKERS ? COUNT_DISPLAYED_SPEAKERS : this.props.speakers.length;
+            for (let i = 0; i < LIMIT; i++) {
                 speakers.push(this.getRandomSpeaker(speakers));
             }
         }
@@ -31,6 +32,8 @@ class SomeSpeakersPresentation extends Component {
     }
 
     render() {
+        const randomSpeakers = this.getRandomSpeakers();
+        console.log(randomSpeakers)
         return (
             <div className="row speakers text-center">
                 <div className="column small-24 sectionTitle">
@@ -40,20 +43,28 @@ class SomeSpeakersPresentation extends Component {
 
                 <div className="column small-24 speakers-container">
                     <div className="row align-center speakers-container-content">
-                        {this.getRandomSpeakers().map((speaker, i) => {
+                        {randomSpeakers.slice(0, 2).map((speaker, i) => {
                             return (<div className="column small-4 align-center speakers-container-content-item" key={`'speaker_${i}`}>
                                 <div className="column auto align-center speakers-container-content-item-picture">
                                     <img src={speaker['Avatar url']} />
                                 </div>
                                 <div className="column auto align-center speakers-container-content-item-text">
                                     <h6>{speaker['Prénom']} {speaker['Nom']}</h6>
-                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio']}</div>
+                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio'].substring(0,150)}...</div>
                                     <div>
-                                        <hr/>
-                                        <span>{speaker['Twitter']}<i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></span>
-                                        <span>{speaker['Linkedin']}<i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></span>
-                                        <span>{speaker['Google+']}<i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></span>
-                                        <span>{speaker['Blog']}<i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></span>
+                                        <hr />
+                                        {speaker['Twitter'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></span>
+                                        }
+                                        {speaker['Linkedin'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></span>
+                                        }
+                                        {speaker['Google+'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></span>
+                                        }
+                                        {speaker['Blog'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></span>
+                                        }
                                     </div>
                                 </div>
                             </div>
@@ -64,17 +75,25 @@ class SomeSpeakersPresentation extends Component {
 
                 <div className="column small-24 speakers-container">
                     <div className="row align-center speakers-container-content">
-                        {this.getRandomSpeakers().map((speaker, i) => {
+                        {randomSpeakers.slice(-2).map((speaker, i) => {
                             return (<div className="column small-4 align-center speakers-container-content-item" key={`'speaker_${i}`}>
                                 <div className="column auto align-center speakers-container-content-item-text">
                                     <h6>{speaker['Prénom']} {speaker['Nom']}</h6>
-                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio']}</div>
+                                    <div className="speakers-container-content-item-text-bio">{speaker['Bio'].substring(0,150)}...</div>
                                     <div>
-                                        <hr/>
-                                        <span>{speaker['Twitter']}<i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></span>
-                                        <span>{speaker['Linkedin']}<i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></span>
-                                        <span>{speaker['Google+']}<i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></span>
-                                        <span>{speaker['Blog']}<i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></span>
+                                        <hr />
+                                        {speaker['Twitter'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></span>
+                                        }
+                                        {speaker['Linkedin'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></span>
+                                        }
+                                        {speaker['Google+'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></span>
+                                        }
+                                        {speaker['Blog'] &&
+                                            <span><i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></span>
+                                        }
                                     </div>
                                 </div>
                                 <img className="column auto align-center speakers-container-content-item-picture"
