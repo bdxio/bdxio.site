eeb8ca8bcbbb6682a0098823b79db1c92099375d
Magali Damourette <magali.damourette@gmail.com>
2018-04-08 17:23:16 +0200

    Adaptation of the map iframe height

--- src/app/components/common/IframeContainer.component.js
+++ src/app/components/common/IframeContainer.component.js
@@ -7,9 +7,7 @@ class IframeContainer extends Component {
 
     render() {
         return (
-            <div>
-                <iframe src={this.props.src} height={this.props.height} width={this.props.width}/>
-            </div>
+            <iframe src={this.props.src} height={this.props.height} width={this.props.width}/>
         );
     }
 }
