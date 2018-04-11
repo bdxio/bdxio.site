ab4b9cb6d3af6d892a87fe27de6985e02451b307
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-11 16:07:59 +0200

    Team DOM

--- src/app/pages/Team.page.js
+++ src/app/pages/Team.page.js
@@ -13,14 +13,32 @@ class Team extends Component {
     render() {
         const { Orgas } = this.props.gsheet;
         return (
-            <div>
-                <Header title="LA TEAM" />
+            <div className="row team text-center">
+                <div className="column small-24 sectionTitle">
+                    <h5 className="sectionTitle">LA TEAM</h5>
+                </div>
+
                 <div className="row">
                     {Orgas.map((member, i) => (
                         <TeamCard key={`member_${i}`} member={member} />
                     ))}
                 </div>
+
+                <div className="column small-24 team-container">
+                    <div className="row align-center team-container-content">
+                        <div className="column small-4 align-center team-container-content-item">
+                            <div className="column auto align-center team-container-content-item-picture">
+                                <img src="" />
+                            </div>
+                            <div className="column auto align-center team-container-content-item-text">
+                                <h6>PRENON NOM</h6>
+                                <div className="team-container-content-item-text-bio">BIO</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
