3470b7ce8b80870016f4316b14e9ed61468415cb
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-15 22:46:12 +0200

    skin team

--- src/app/components/team/TeamCard.component.js
+++ src/app/components/team/TeamCard.component.js
@@ -9,29 +9,29 @@ class TeamCard extends Component {
     render() {
         const { member } = this.props;
         return (
-            <div className="column small-4 align-center speakers-container-content-item">
-                <img className="column auto align-center speakers-container-content-item-picture"
+            <div className="column small-4 align-center team-content-item">
+                <img className="column auto align-center team-content-item-picture"
                     src={member['Avatar url']} alt={member['Nom']} />
-
-                <div className="column auto align-center speakers-container-content-item-text">
+                <div className="column auto text-center team-content-item-text">
                     <h6>{member['Prénom']} {member['Nom']}</h6>
-                    <div className="speakers-container-content-item-text-bio">{member['Bio']}</div>
+                    <div className="team-content-item-text-bio">{member['Bio']}</div>
                     <div>
                         <hr />
                         {member['Twitter'] &&
-                            <span><a href={`https://twitter.com/${member['Twitter']}`}><i className="speakers-container-content-item-text-socialNetwork fa fa-twitter"></i></a></span>
+                            <span><a href={`https://twitter.com/${member['Twitter']}`} target="_blank"><i className="team-content-item-text-socialNetwork fa fa-twitter"></i></a></span>
                         }
                         {member['Linkedin'] &&
-                            <span><a href={member['Linkedin']}><i className="speakers-container-content-item-text-socialNetwork fa fa-linkedin"></i></a></span>
+                            <span><a href={member['Linkedin']} target="_blank"><i className="team-content-item-text-socialNetwork fa fa-linkedin"></i></a></span>
                         }
                         {member['Google+'] &&
-                            <span><a href={member['Google+']}><i className="speakers-container-content-item-text-socialNetwork fa fa-google-plus"></i></a></span>
+                            <span><a href={member['Google+']} target="_blank"><i className="team-content-item-text-socialNetwork fa fa-google-plus"></i></a></span>
                         }
                         {member['Blog'] &&
-                            <span><a href={member['Blog']}><i className="speakers-container-content-item-text-socialNetwork fa fa-globe"></i></a></span>
+                            <span><a href={member['Blog']} target="_blank"><i className="team-content-item-text-socialNetwork fa fa-globe"></i></a></span>
                         }
                     </div>
                 </div>
+                <span className="team-content-item-arrow"></span>
             </div>
         )
     }
--- src/app/pages/Team.page.js
+++ src/app/pages/Team.page.js
@@ -14,20 +14,22 @@ class Team extends Component {
         const { Orgas } = this.props.gsheet;
         return (
             <div>
-                <Header title="LA TEAM" />
-                <div className="row auto align-center">
+                <Header title="LA TEAM"/>
+                <div className="row auto align-center team">
                     <div className="column small-12">
                         <h4>/ LE BUREAU</h4>
-                        <div className="row">
+                        {/* TODO fix class "team-open" like in component speakers for see all the content of the person */} 
+                        <div className="row team-row-1 team-content">
                             {Orgas.map((member, i) => (
                                 <TeamCard key={`member_${i}`} member={member} />
                             ))}
                         </div>
                     </div>
 
-                    <div className="column small-12">
+                    <div className="column small-12 team">
                         <h4>/ LES AUTRES MEMBRES</h4>
-                        <div className="row">
+                        {/* TODO fix class "team-open" like in component speakers for see all the content of the person */} 
+                        <div className="row team-row-2 team-content">
                             {Orgas.map((member, i) => (
                                 <TeamCard key={`member_${i}`} member={member} />
                             ))}
