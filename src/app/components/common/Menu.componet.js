0735176ca89a15bda4927a0340a3148ff2749bd0
Estelle Sifre <estellesifre@MacBook-Pro-de-Estelle.local>
2018-04-15 18:31:14 +0200

    TODO

--- src/app/components/common/Menu.componet.js
+++ src/app/components/common/Menu.componet.js
@@ -14,7 +14,7 @@ class Menu extends Component {
                 </span>
                 <div className="main-menu">
                 <ul className="columns align-center main-menu-item">
-                {/*  FIXME added selected class on link, thank you for adding a link for this commande work*/}
+                {/*  TODO added selected class on link, thank you for adding a link for this commande work*/}
                     <li className="shrink menu-item-content"><Link className="selected" to="/home">Accueil</Link></li>
                     <li className="shrink menu-item-content"><Link to="/team">La team</Link></li>
                     <li className="shrink menu-item-content"><Link to="/news">Actualités</Link></li>
