f7040916954746141f41bf9765331f67d052ba1f
jbehuet <jbehuet@gmail.com>
2018-04-17 23:27:31 +0200

    Add mailchimp subscribe

--- src/app/components/common/Footer.component.js
+++ src/app/components/common/Footer.component.js
@@ -1,22 +1,80 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import jsonp from 'jsonp';
 
 class Footer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            email: '',
+            status: null,
+            message: 'Messageeeeee'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.subscribe = this.subscribe.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    subscribe() {
+        const url = 'https://bdx.us10.list-manage.com/subscribe/post-json?u=3fdd02789fbab2f90b81652a3&id=760c78a462&EMAIL=' + this.state.email;
+
+        jsonp(
+            url,
+            {
+                param: "c"
+            },
+            (err, data) => {
+                console.log("err, data", err, data);
+                if (err) {
+                    this.setState({
+                        status: "error",
+                        message: err
+                    });
+                } else if (data.result !== "success") {
+                    this.setState({
+                        status: "error",
+                        message: data.msg
+                    });
+                } else {
+                    this.setState({
+                        email: '',
+                        status: "success",
+                        message: data.msg
+                    });
+                }
+                //Clear message
+                setTimeout(() => this.setState({ status: null, message: '' }), 2000)
+            }
+        )
+
     }
 
     render() {
         const { style } = this.props;
+        const { email, status, message } = this.state;
         return (
             <div className="footer" style={style}>
                 <div className="footer-signIn">
                     <div className="info">Vous souhaitez être tenu(e) informé(e) ?<br />Inscrivez-vous à la newsletter</div>
                     <div className="email">
-                        <input  type="text" placeholder={'Email'} />
-                        <button type={'button'} className={'button small white'}>S'inscrire</button>
+                        <input type="email"
+                            placeholder={'Email'}
+                            value={email}
+                            onChange={this.handleChange} />
+                        <button type={'button'}
+                            className={'button small white'}
+                            onClick={this.subscribe}
+                        >
+                            S'inscrire
+                        </button>
                     </div>
+                     {/* {!!status && */}
+                     <div>{message}</div>
+                        {/* } */}
                 </div>
 
                 <div className="columns footer-item">
