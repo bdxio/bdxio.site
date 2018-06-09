61fe0f95ce71001745ab1dccb59fabbd63bf0abf
jbehuet <jbehuet@gmail.com>
2018-06-09 16:39:52 +0200

    Edit sponsors bar

--- src/app/components/common/SponsorsBar.component.js
+++ src/app/components/common/SponsorsBar.component.js
@@ -1,34 +1,69 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class SponsorsBar extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const {partners} = this.props;
-        return (
-            <section className="row sponsorBar">
-                <img className="homeFlottant" src="img/svg/sponsor_flottant.svg"/>
-                <div className="columns shrink text-center">
-                    <h5 className="sectionTitle">Nos sponsors Impériaux</h5>
-                </div>
-
-                <div className="columns auto text-center">
-                    {partners.map((p, i) => (
-                        <img className="columns small-3" key={`sb_${i}`} src={p['Logo url']} alt={p['Société']}
-                             width="100"/>
-                    ))}
-                </div>
-                <img className="homeFlottant1" src="img/svg/footer_flottant.svg"/>
-            </section>
-        );
-    }
+  render() {
+    const { partners } = this.props;
+    return (
+      <section className="row sponsorBar">
+        <img className="homeFlottant" src="img/svg/sponsor_flottant.svg" />
+        <div className="columns shrink text-center">
+          <h5 className="title">Nos sponsors Impériaux</h5>
+        </div>
+        <div className="row">
+          <div className="columns auto text-center">
+            {partners
+              .filter((p, i) => i === 0)
+              .map((p, i) => (
+                <img
+                  key={`sb_${i}`}
+                  src={p["Logo url"]}
+                  alt={p["Société"]}
+                  width={p["Hauteur logo"] || 150}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="row">
+          <div className="columns auto text-center">
+            {partners
+              .filter((p, i) => i > 0 && i <= 5)
+              .map((p, i) => (
+                <img
+                  key={`sb_${i}`}
+                  src={p["Logo url"]}
+                  alt={p["Société"]}
+                  width={p["Hauteur logo"] || 150}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="row">
+          <div className="columns auto text-center">
+            {partners
+              .filter((p, i) => i > 5)
+              .map((p, i) => (
+                <img
+                  key={`sb_${i}`}
+                  src={p["Logo url"]}
+                  alt={p["Société"]}
+                  width={p["Hauteur logo"] || 150}
+                />
+              ))}
+          </div>
+        </div>
+        <img className="homeFlottant1" src="img/svg/footer_flottant.svg" />
+      </section>
+    );
+  }
 }
 
 SponsorsBar.propTypes = {
-    partners: PropTypes.array.isRequired,
-}
+  partners: PropTypes.array.isRequired
+};
 
-export default SponsorsBar
+export default SponsorsBar;
