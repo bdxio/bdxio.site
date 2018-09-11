9ef9aa0f444be5f6ec35beda449e7c7ed4861f8b
jbehuet <jbehuet@gmail.com>
2018-09-11 15:38:16 +0200

    Disable get twitter avatar

--- src/app/components/common/UserCard.component.js
+++ src/app/components/common/UserCard.component.js
@@ -14,16 +14,16 @@ class UserCard extends Component {
     const { user } = this.props;
     this.getImageSize(user["Avatar url"]).then(size => {
       if (size.width < 150 || size.height < 150) {
-        if (user["Twitter"]) {
-          user["Avatar url"] = `https://twitter.com/${
-            user["Twitter"]
-          }/profile_image?size=original`;
-        } else {
+        // if (user["Twitter"]) {
+        //   user["Avatar url"] = `https://twitter.com/${
+        //     user["Twitter"]
+        //   }/profile_image?size=original`;
+        // } else {
           user["Avatar url"] =
             user["gender"] === "male"
               ? "/img/svg/bdxio_male_speaker.svg"
               : "/img/svg/bdxio_female_speaker.svg";
-        }
+        //}
       }
       this.setState({ user });
     });
