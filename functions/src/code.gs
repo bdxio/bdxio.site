5ba7f784e864ec05e3214f29911ffa3eab1ab508
Etienne Grandier-Vazeille <e.grandier-vazeille@groupeonepoint.com>
2018-09-17 21:20:32 +0200

    feat: add spreadsheet sync by google api and gs script on target SS

---
+++ functions/src/code.gs
@@ -0,0 +1,43 @@
+function syncToFirebase () {
+  var sheets = SpreadsheetApp.getActive().getSheets();
+  var res = {};
+  sheets.forEach(function(sheet) {
+    res[sheet.getName()] = sheetToObject(sheet)
+    Logger.log(sheet.getName());
+  });
+  
+  var url = 'https://us-central1-bdxio-website.cloudfunctions.net/saveData';
+  post(res, url);
+}
+
+function post(data, url) {
+  var options = {
+    'method' : 'post',
+    'contentType': 'application/json',
+    'payload' : JSON.stringify(data)
+  };
+  UrlFetchApp.fetch(url, options);
+}
+
+function sheetToObject(sheet){
+  var output = [];
+  
+  var rows = sheet.getDataRange();
+  var numRows = rows.getNumRows();
+  var numCols = rows.getNumColumns();
+  var values = rows.getValues();
+  
+  var header = values[0];
+  for (var i = 1; i < numRows; i++) {
+    var row = values[i];
+    var name = row[0];
+    var obj = {};
+    for (var j = 0; j < numCols; j++) {
+      var key = header[j];
+      var value = row[j];
+      obj[key] = value;
+    }
+    output.push(obj);
+  }
+  return output;
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as Tabletop from 'tabletop';
-admin.initializeApp();
+import { google } from 'googleapis';
+const sheets = google.sheets('v4');
+import { API_KEY } from './key';
 
-// const firestore = new Firestore();
-// const settings = {/* your settings... */ timestampsInSnapshots: true };
-// firestore.settings(settings);
+admin.initializeApp();
+const settings = { timestampsInSnapshots: true };
+admin.firestore().settings(settings);
 
 const CONFIG = {
   gSheets: {
@@ -18,6 +20,7 @@ const CONFIG = {
   collectionsId: {
     Orgas: 'Nom',
     Speakers18: ['Nom', 'Prénom'],
+    Speakers: ['Nom', 'Prénom'],
     Speakers17: ['Nom', 'Prénom'],
     Sponsors: 'Société',
     News: 'ID',
@@ -25,7 +28,11 @@ const CONFIG = {
   },
 };
 
-function arrayKeyValueToObject(array, key = 'Propriété', value = 'Valeur') {
+function arrayKeyValueToObject(
+  array: Array<any>,
+  key = 'Propriété',
+  value = 'Valeur',
+): Object {
   const res = {};
   array.forEach(element => {
     try {
@@ -37,7 +44,24 @@ function arrayKeyValueToObject(array, key = 'Propriété', value = 'Valeur') {
   return res;
 }
 
-function spreadsheetToPOJO(spreadsheetArray) {
+function spreadsheetToPOJO(spreadsheetArray: Object): Object {
+  const values = Object.keys(spreadsheetArray)
+    .map(page => {
+      return { [page]: spreadsheetArray[page] };
+    })
+    .reduce((acc, cur, i) => {
+      const key = Object.keys(cur)[0];
+      if (key === 'Config') {
+        acc[key] = arrayKeyValueToObject(cur[key]);
+      } else {
+        acc[key] = cur[key];
+      }
+      return acc;
+    }, {});
+  return values;
+}
+
+function spreadsheetTableTopToPOJO(spreadsheetArray) {
   const values = Object.keys(spreadsheetArray)
     .map(page => {
       return { [page]: spreadsheetArray[page].elements };
@@ -54,7 +78,11 @@ function spreadsheetToPOJO(spreadsheetArray) {
   return values;
 }
 
-function getID(object, label, collectionsId = CONFIG.collectionsId) {
+function getID(
+  object: Object,
+  label: string,
+  collectionsId = CONFIG.collectionsId,
+): string {
   if (Array.isArray(collectionsId[label])) {
     let res = '';
     collectionsId[label].forEach(part => {
@@ -68,49 +96,156 @@ function getID(object, label, collectionsId = CONFIG.collectionsId) {
   return object['ID'];
 }
 
-export const spreadsheet = functions.https.onRequest((request, response) => {
+function saveDate(data: Object, promises: Array<Promise<any>>): void {
+  Object.keys(data).forEach(key => {
+    const element = data[key];
+    if (CONFIG.ignore.indexOf(key) > -1) {
+      return null;
+    }
+
+    if (Array.isArray(element) && element.length > 0) {
+      console.log(key);
+      const batch = admin.firestore().batch();
+      const refCollection = admin
+        .firestore()
+        .collection(CONFIG.year)
+        .doc(CONFIG.arrayCollection)
+        .collection(key);
+      element.forEach(obj => {
+        batch.set(refCollection.doc(getID(obj, key) + ''), obj);
+      });
+      promises.push(batch.commit());
+    } else {
+      promises.push(
+        admin
+          .firestore()
+          .collection(CONFIG.year)
+          .doc(key)
+          .set(element),
+      );
+    }
+  });
+}
+
+function getSheets(jwtClient): Promise<Object> {
+  const sheetsResult = {};
+  const requestSheets = {
+    auth: jwtClient,
+    spreadsheetId: CONFIG.gSheets.published_data,
+  };
   const promises = [];
+
+  return new Promise(function(resolve, reject) {
+    sheets.spreadsheets.get(requestSheets, (err, response) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      response.data.sheets.forEach(sheet => {
+        promises.push(
+          getSheetValues(jwtClient, sheet).then(sheetValues => {
+            sheetsResult[sheet.properties.title] = sheetValues;
+            return sheetValues;
+          }),
+        );
+      });
+
+      Promise.all(promises)
+        .then(() => {
+          resolve(sheetsResult);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  });
+}
+
+function getSheetValues(jwtClient, sheet): Promise<Array<any>> {
+  const title = sheet.properties.title;
+  const sheetValues = [];
+  const requestSheet = {
+    auth: jwtClient,
+    spreadsheetId: CONFIG.gSheets.published_data,
+    range: title,
+  };
+  return new Promise(function(resolve, reject) {
+    sheets.spreadsheets.values.get(requestSheet, (err, response) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      let labels;
+      response.data.values.forEach((row, indexRow) => {
+        if (indexRow === 0) {
+          labels = row;
+        } else {
+          sheetValues[indexRow - 1] = {};
+          row.forEach((column, indexColumn) => {
+            sheetValues[indexRow - 1][labels[indexColumn]] = column;
+          });
+        }
+      });
+      resolve(sheetValues);
+    });
+  });
+}
+
+export const spreadsheetSyncByApi = functions.https.onRequest(
+  (request, response) => {
+    const jwtClient = google.auth.fromAPIKey(API_KEY);
+    getSheets(jwtClient)
+      .then(sheetsValues => {
+        const promises = [];
+        const res = spreadsheetToPOJO(sheetsValues);
+        saveDate(res, promises);
+        return Promise.all(promises);
+      })
+      .then(() => {
+        response.send('🚀 Website config is up to date ! 🎉');
+      })
+      .catch(error => {
+        response.send('😱 Something went wrong 👉' + error);
+      });
+  },
+);
+
+export const spreadsheetSyncByTabletop = functions.https.onRequest((request, response) => {
+  const promises = [];
+
   Tabletop.init({
     key: CONFIG.gSheets.published_data,
     callback: data => {
-      const res = spreadsheetToPOJO(data);
+      const res = spreadsheetTableTopToPOJO(data);
+      saveDate(res, promises);
 
-      Object.keys(res).forEach(key => {
-        const element = res[key];
-        if (CONFIG.ignore.indexOf(key) > -1) {
-          return null;
-        }
-
-        if (Array.isArray(element) && element.length > 0) {
-          const batch = admin.firestore().batch();
-          const refCollection = admin
-            .firestore()
-            .collection(CONFIG.year)
-            .doc(CONFIG.arrayCollection)
-            .collection(key);
-          element.forEach(obj => {
-            batch.set(refCollection.doc(getID(obj, key)), obj);
-          });
-          promises.push(
-            batch
-              .commit(),
-          );
-        } else {
-          promises.push(
-            admin
-              .firestore()
-              .collection(CONFIG.year)
-              .doc(key)
-              .set(element),
-          );
-        }
-      });
-
-      Promise.all(promises).then(() => {
-        response.send('🚀 Website config is up to date ! 🎉');
-      }).catch((error)=> {
-        response.send('😱 Something went wrong 👉' + error);
-      })
+      Promise.all(promises)
+        .then(() => {
+          response.send('🚀 Website config is up to date ! 🎉');
+        })
+        .catch(error => {
+          response.send('😱 Something went wrong 👉' + error);
+        });
     },
   });
+});
+
+/**
+ * Trigger by code.gs on the taget spreadsheet
+ */
+export const saveData = functions.https.onRequest(async (request, response) => {
+  const data = request.body;
+  const promises = [];
+
+  const res = spreadsheetToPOJO(data);
+  saveDate(res, promises);
+
+  Promise.all(promises)
+    .then(() => {
+      response.send('🚀 Website config is up to date ! 🎉');
+    })
+    .catch(error => {
+      response.send('😱 Something went wrong 👉' + error);
+    });
 });
