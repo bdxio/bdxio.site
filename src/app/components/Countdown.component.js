7cba1d14ffa85ba2e7e1f9cc81fde33cddd5fc40
jbehuet <jbehuet@gmail.com>
2018-11-06 11:21:47 +0100

    Hide countdown in date after event date

--- src/app/components/Countdown.component.js
+++ src/app/components/Countdown.component.js
@@ -1,79 +1,82 @@
-import React, { Component } from 'react';
-import Moment from 'moment';
-import { Constants } from '../constants';
+import React, { Component } from "react";
+import Moment from "moment";
+import { Constants } from "../constants";
 
 class Countdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      intervalId: null,
+      timeRemaining: null,
+      ended: false
+    };
+    this.tick = this.tick.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            intervalId: null,
-            timeRemaining: null
-        };
-        this.tick = this.tick.bind(this);
+  componentWillMount() {
+    const intervalId = setInterval(this.tick, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  tick() {
+    const timeRemaining = Moment.duration(
+      Constants.eventDate.diff(Moment.now())
+    );
+    let ended = false;
+    if (
+      timeRemaining._data.years <= 0 &&
+      timeRemaining._data.months <= 0 &&
+      timeRemaining._data.days <= 0 &&
+      timeRemaining._data.hours <= 0 &&
+      timeRemaining._data.minutes <= 0 &&
+      timeRemaining._data.seconds <= 0
+    ) {
+      clearInterval(this.state.intervalId);
+      ended = true;
     }
+    this.setState({ timeRemaining, ended });
+  }
 
-    componentWillMount() {
-        const intervalId = setInterval(this.tick, 1000);
-        this.setState({ intervalId });
-    }
+  render() {
+    const { timeRemaining, ended } = this.state;
+    return (
+      <div className="row">
+        {!ended &&
+          timeRemaining && (
+            <div className={"columns small-12 align-center countdown"}>
+              <div className="small-2 medium-1 text-center">
+                <h2 className="">{timeRemaining._data.months}</h2>
+                <div className="">mois</div>
+              </div>
 
-    componentWillUnmount() {
-        clearInterval(this.state.intervalId);
-    }
+              <div className="small-2 medium-1 text-center countdown-border">
+                <h2 className="">{timeRemaining._data.days}</h2>
+                <div className="">jours</div>
+              </div>
 
-    tick() {
-        this.setState({
-            timeRemaining: Moment.duration(Constants.eventDate.diff(Moment.now()))
-        })
-    }
+              <div className="small-2 medium-1 text-center">
+                <h2 className="">{timeRemaining._data.hours}</h2>
+                <div className="">heures</div>
+              </div>
 
-    render() {
-        const { timeRemaining } = this.state;
-        return (
-            <div className="row">
-                <div className={'columns small-12 align-center countdown'}>
-                    <div className="small-2 medium-1 text-center">
-                        {timeRemaining &&
-                            <h2 className="">{timeRemaining._data.months}</h2>
-                        }
-                        <div className="">mois</div>
-                    </div>
+              <div className="small-2 medium-1 text-center countdown-border">
+                <h2 className="">{timeRemaining._data.minutes}</h2>
+                <div className="">minutes</div>
+              </div>
 
-                    <div className="small-2 medium-1 text-center countdown-border">
-                        {timeRemaining &&
-                            <h2 className="">{timeRemaining._data.days}</h2>
-                        }
-                        <div className="">jours</div>
-                    </div>
-
-                    <div className="small-2 medium-1 text-center">
-
-                        {timeRemaining &&
-                            <h2 className="">{timeRemaining._data.hours}</h2>
-                        }
-                        <div className="">heures</div>
-                    </div>
-
-                    <div className="small-2 medium-1 text-center countdown-border">
-
-                        {timeRemaining &&
-                            <h2 className="">{timeRemaining._data.minutes}</h2>
-                        }
-                        <div className="">minutes</div>
-                    </div>
-
-                    <div className="small-2 medium-1 text-center">
-
-                        {timeRemaining &&
-                            <h2 className="">{timeRemaining._data.seconds}</h2>
-                        }
-                        <div className="">secondes</div>
-                    </div>
-                </div>
+              <div className="small-2 medium-1 text-center">
+                <h2 className="">{timeRemaining._data.seconds}</h2>
+                <div className="">secondes</div>
+              </div>
             </div>
-        );
-    }
+          )}
+      </div>
+    );
+  }
 }
 
-export default Countdown
+export default Countdown;
