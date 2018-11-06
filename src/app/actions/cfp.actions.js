c7a89e36136cf5b8309071e1dbc5cd1248b7eebf
jbehuet <jbehuet@gmail.com>
2018-11-06 18:27:56 +0100

    Update tracks list

--- src/app/actions/cfp.actions.js
+++ src/app/actions/cfp.actions.js
@@ -5,15 +5,31 @@ export const Types = {
 };
 
 export class Actions {
-
   static getTalks() {
     return dispatch => {
-      get("https://cfp.bdx.io/api/conferences/BdxIO2018/talks").then(res => {
+      get(
+        "https://cfp.bdx.io/api/conferences/BdxIO2018/schedules/friday/"
+      ).then(res => {
+        const { slots } = res.data;
+        const talks = slots.reduce((prev, curr) => {
+          if (curr.talk && curr.talk.talkType !== "Keynote")
+            prev.push(curr.talk);
+          return prev;
+        }, []);
         return dispatch({
           type: Types.GET_TALKS,
-          payload: { talks: res.data }
+          payload: { talks }
         });
       });
     };
+
+    // return dispatch => {
+    //   get("https://cfp.bdx.io/api/conferences/BdxIO2018/talks").then(res => {
+    //     return dispatch({
+    //       type: Types.GET_TALKS,
+    //       payload: { talks: res.data }
+    //     });
+    //   });
+    // };
   }
 }
