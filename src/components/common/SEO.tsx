0b5cd46ea8663fee1b122cb78a0c5d30fbe0bcc4
Jerome BEHUET <jbehuet@gmail.com>
2020-03-05 11:54:53 +0100

    add favicons

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -9,7 +9,7 @@ interface PropsType {
   meta?: [];
 }
 
-const SEO = ({ title, description, lang = 'en', meta = [] }: PropsType) => {
+const SEO = ({ title, description, lang = 'fr', meta = [] }: PropsType) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -63,8 +63,67 @@ const SEO = ({ title, description, lang = 'en', meta = [] }: PropsType) => {
         {
           name: `twitter:description`,
           content: metaDescription
+        },
+        {
+          name: `msapplication-TileColor`,
+          content: '#ffffff'
+        },
+        {
+          name: `msapplication-TileImage`,
+          content: '/favicon/ms-icon-144x144.png'
+        },
+        {
+          name: `theme-color`,
+          content: '#ffffff'
         }
       ].concat(meta)}
+      link={[
+        ...[57, 60, 72, 76, 114, 120, 144, 152, 180].map((size: number) => {
+          return {
+            rel: 'apple-touch-icon',
+            sizes: `${size}x${size}`,
+            href: `/favicon/apple-icon-${size}x${size}.png`
+          };
+        }),
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon/favicon.ico'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon/favicon.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: `192x192`,
+          href: `/favicon/android-icon-192x192.png`
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: `32x32`,
+          href: `/favicon/favicon-32x32.png`
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: `96x96`,
+          href: `/favicon/favicon-96x96.png`
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: `16x16`,
+          href: `/favicon/favicon-16x16.png`
+        },
+        {
+          rel: 'manifest',
+          href: `/favicon/manifest.json`
+        }
+      ]}
     />
   );
 };
