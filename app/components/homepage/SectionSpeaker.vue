88f92d3c2fad09d89f5b3152f71c4b083349ed21
Hub <hubert.moncenis@comptalib.com>
2022-05-20 17:27:11 +0200

    :lipstick: width 100% on section speaker

--- app/components/homepage/SectionSpeaker.vue
+++ app/components/homepage/SectionSpeaker.vue
@@ -1,14 +1,10 @@
 <template>
   <section class="section section-speaker">
     <div>
-      <section-title
-        section
-        tag="h2"
-      >speakers</section-title>
-      <section-title
-        tag="h3"
-        class="title"
-      >Vous aussi, rejoignez le navire !</section-title>
+      <section-title section tag="h2">speakers</section-title>
+      <section-title tag="h3" class="title"
+        >Vous aussi, rejoignez le navire !</section-title
+      >
       <p>
         Vous avez un talk à proposer ? Des idées à partager ?
         <br />Rejoignez notre équipe de speakers pour cette édition 2022.
@@ -31,5 +27,6 @@ export default {
   justify-content: space-between;
   align-items: flex-start;
   background-color: $secondary;
+  width: 100%;
 }
 </style>
