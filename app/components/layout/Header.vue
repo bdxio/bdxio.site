44f702607d54f859e9164ba5259fd4a8c9952583
Hub <hubert.moncenis@comptalib.com>
2022-05-20 18:19:56 +0200

    :art: reowrk responsive menu display + show navigation in footer

--- app/components/layout/Header.vue
+++ app/components/layout/Header.vue
@@ -1,12 +1,6 @@
 <template>
-  <header
-    class="header"
-    :class="color"
-  >
-    <NuxtLink
-      class="logo"
-      to="/"
-    >
+  <header class="header" :class="color">
+    <NuxtLink class="logo" to="/">
       <img
         src="~/assets/img/bdxio_logo.png"
         alt="Logo de l'association BDX.IO"
@@ -33,34 +27,14 @@
       </ul>
     </nav>
 
-    <div class="hidden-m">
+    <div class="hidden-m" v-if="showMenu">
       <IconBurger
         :open="mobileOpen"
         class="header__burger"
         @click="mobileOpen = !mobileOpen"
       />
-      <nav
-        v-if="showMenu"
-        class="header__nav"
-        :class="propClasses"
-      >
-        <ul>
-          <li>
-            <NuxtLink to="/">Accueil</NuxtLink>
-          </li>
-          <!-- <li>
-          <NuxtLink to="/schedule">Programme</NuxtLink>
-        </li> -->
-          <li>
-            <NuxtLink to="/sponsors">Sponsors</NuxtLink>
-          </li>
-          <!-- <li>
-            <NuxtLink to="/team">L'équipe</NuxtLink>
-          </li> -->
-          <!-- <li>
-          <button class="bdx-button">Billetterie</button>
-        </li> -->
-        </ul>
+      <nav class="header__nav" :class="propClasses">
+        <Navigation />
       </nav>
     </div>
   </header>
@@ -68,16 +42,18 @@
 
 <script>
 import IconBurger from "~/components/layout/IconBurger.vue";
+import Navigation from "./Navigation.vue";
 
 export default {
   name: "Header",
   components: {
     IconBurger,
+    Navigation,
   },
   data() {
     return {
       mobileOpen: false,
-      showMenu: false,
+      showMenu: true,
     };
   },
   props: {
@@ -110,145 +86,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: $spc-m 0px;
-
-  &.light {
-    background-color: $primary-dark;
-  }
-
-  &.dark {
-    background-color: $light-font;
-  }
-
-  @include mobileFirst(m) {
-    padding: 30px 50px;
-    justify-content: space-between;
-  }
-
-  &__burger {
-    position: absolute;
-    @include z-index(upper);
-    right: $spc-m;
-    top: $spc-m;
-    cursor: pointer;
-  }
-
-  &__nav {
-    position: absolute;
-    @include z-index(design);
-    width: 100%;
-    top: 0;
-    right: 100%;
-    bottom: 0;
-    background-color: $primary-dark;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    &.mobile-open {
-      right: 0;
-    }
-
-    ul {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-
-      li {
-        margin: 0; //50px;
-
-        a {
-          text-decoration: none;
-
-          &.nuxt-link-exact-active {
-            position: relative;
-
-            &:after {
-              content: "";
-              width: 90px;
-              height: 30px;
-              display: block;
-              position: absolute;
-              bottom: -30px;
-              left: 50%;
-              transform: translateX(-50%);
-              background: url("~/assets/img/drawings/blue_underline.png")
-                no-repeat center / cover;
-            }
-          }
-        }
-
-        &:not(:last-of-type) {
-          margin-bottom: 100px;
-        }
-
-        &:not(:first-of-type) a {
-          display: flex;
-          align-items: center;
-        }
-
-        a {
-          color: $light-font;
-        }
-
-        img {
-          height: 3em;
-          transition: 0.3s;
-
-          &:hover {
-            transform: scale(1.1);
-          }
-        }
-      }
-    }
-
-    &.dark a {
-      color: $primary-dark;
-    }
-
-    &.light a {
-      color: $light-font;
-    }
-
-    @include mobileFirst(m) {
-      position: initial;
-      display: block;
-      width: 100%;
-
-      ul {
-        justify-content: flex-end;
-        flex-direction: row;
-
-        li {
-          margin-right: $spc-xl;
-
-          &:not(:last-of-type) {
-            margin-bottom: 0;
-          }
-        }
-      }
-    }
-  }
-
-  .mobile {
-    display: block;
-    @include mobileFirst(m) {
-      display: none;
-    }
-  }
-  .desktop {
-    display: none;
-    @include mobileFirst(m) {
-      display: block;
-    }
-  }
-}
-</style>
