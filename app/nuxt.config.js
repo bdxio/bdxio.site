8945dc433f33d1cbffcb8df766ab37ce84237099
Hub <hubert.moncenis@comptalib.com>
2022-05-26 20:23:51 +0200

    🎨 add favicons

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -17,8 +17,37 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
+      { name: "msapplication-TileColor", content: "#da532c" },
+      { name: "theme-color", content: "#ffffff" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
+      {
+        rel: "manifest",
+        href: "/favicons/site.webmanifest",
+      },
+      {
+        rel: "mask-icon",
+        href: "/favicons/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
