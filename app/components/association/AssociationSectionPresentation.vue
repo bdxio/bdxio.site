8c814824e34808202b5001aac8a9024d80e0e9ad
Hub <hubert.moncenis@comptalib.com>
2022-06-15 12:06:53 +0200

    ✨ display asso page w/ first section and pics

--- app/components/association/AssociationSectionPresentation.vue
+++ app/components/association/AssociationSectionPresentation.vue
@@ -11,21 +11,25 @@
     </header>
     <flex-container
       tag="section"
-      class="section-presentation__content"
+      class="section-presentation__body"
       gutter-l
+      align-center-xs
     >
-      <flex-item xs12 m6>
+      <flex-item xs12 m6 class="section-presentation__body__content">
         <section-title
           section
           tag="h2"
-          class="section-presentation__content__pre-title"
+          class="section-presentation__body__content__pre-title"
           >Qui sommes nous ?</section-title
         >
-        <section-title tag="h2" class="section-presentation__content__title">
+        <section-title
+          tag="h2"
+          class="section-presentation__body__content__title"
+        >
           Un petit groupe de bénévoles <span class="boxed">bordelais</span> !
         </section-title>
 
-        <p class="section-presentation__content__description">
+        <p class="section-presentation__body__content__description">
           Nous avons à cœur de vous proposer un événement fédérateur, convivial
           et de grande ampleur. Notre objectif est de permettre à l’ensemble des
           membres de la communauté IT de se rassembler au sein d’une journée de
@@ -36,11 +40,52 @@
         <nuxt-link
           v-if="showProgramme"
           to="/programme"
-          class="button button-secondary button-secondary--grey"
+          class="button button-secondary button-secondary--dark"
           >Découvrir le programme 2022</nuxt-link
         >
       </flex-item>
-      <flex-item xs12 m6> yo 2 </flex-item>
+      <flex-item xs12 m6 class="section-presentation__body__imgs">
+        <div class="section-presentation__imgs__column">
+          <picture>
+            <source type="image/webp" srcset="/asso/pic-scene-1.webp" />
+            <source type="image/jpeg" srcset="/asso/pic-scene-1.jpg" />
+            <img
+              src="/asso/pic-scene-1.jpg"
+              alt="photo de l'équipe prise sur la scene de l'évènement"
+              class="picture"
+            />
+          </picture>
+          <picture>
+            <source type="image/webp" srcset="/asso/pic-scene-2.webp" />
+            <source type="image/jpeg" srcset="/asso/pic-scene-2.jpg" />
+            <img
+              src="/asso/pic-scene-2.jpg"
+              alt="photo de l'équipe prise sur la scene de l'évènement"
+              class="picture"
+            />
+          </picture>
+        </div>
+        <div class="section-presentation__imgs__column">
+          <picture>
+            <source type="image/webp" srcset="/asso/pic-meeting-1.webp" />
+            <source type="image/jpeg" srcset="/asso/pic-meeting-1.jpg" />
+            <img
+              src="/asso/pic-meeting-1.jpg"
+              alt="photo de l'équipe prise pendant un meeting bdx io"
+              class="picture"
+            />
+          </picture>
+          <picture>
+            <source type="image/webp" srcset="/asso/pic-meeting-2.webp" />
+            <source type="image/jpeg" srcset="/asso/pic-meeting-2.jpg" />
+            <img
+              src="/asso/pic-meeting-2.jpg"
+              alt="photo de l'équipe prise pendant un meeting bdx io"
+              class="picture"
+            />
+          </picture>
+        </div>
+      </flex-item>
     </flex-container>
   </div>
 </template>
@@ -58,6 +103,7 @@ export default {
 
 <style lang="scss" scoped>
 .section-presentation {
+  background-color: $white;
   &__header {
     display: flex;
     flex-direction: column;
@@ -86,36 +132,68 @@ export default {
     }
   }
 
-  &__content {
+  &__body {
     margin-top: 100px;
+    justify-content: space-between;
 
-    &__pre-title {
-      color: $green;
-    }
+    &__content {
+      margin-bottom: 100px;
+      margin-top: $spc-m;
 
-    &__title {
-      margin: $spc-s 0;
+      @include mobileFirst(l) {
+        max-width: 500px;
+        margin-bottom: 0;
+      }
 
-      .boxed {
-        position: relative;
+      @include mobileFirst(xl) {
+        max-width: 650px;
+      }
 
-        &::after {
-          content: "";
-          width: 240px;
-          height: 100px;
-          position: absolute;
-          z-index: -1;
-          background: url("~/assets/img/drawings/green_box.png") center
-            no-repeat;
-          background-size: cover;
-          top: -10px;
-          left: -20px;
+      &__pre-title {
+        color: $green;
+      }
+
+      &__title {
+        margin: $spc-s 0;
+
+        .boxed {
+          position: relative;
+
+          &::after {
+            content: "";
+            width: 240px;
+            height: 100px;
+            position: absolute;
+            z-index: 0;
+            background: url("~/assets/img/drawings/green_box.png") center
+              no-repeat;
+            background-size: cover;
+            top: -10px;
+            left: -20px;
+          }
         }
+      }
+
+      &__description {
+        margin-top: $spc-m;
+      }
+
+      .button {
+        margin-top: $spc-xxxl;
       }
     }
 
-    &__description {
-      margin: $spc-m 0 $spc-xxxl 0;
+    &__imgs {
+      display: flex;
+      gap: $spc-m;
+
+      &__column {
+        width: 50%;
+      }
+      .picture {
+        width: 100%;
+        margin-bottom: $spc-m;
+      }
     }
   }
 }
