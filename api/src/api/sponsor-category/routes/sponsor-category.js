41716283598e04a10b9b0d1455b90b2757a87f9a
Amelie Benoit <amelie.benoit@busbud.com>
2022-07-09 08:14:04 +0200

    chore: add Sponsors in Strapi

---
+++ api/src/api/sponsor-category/routes/sponsor-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sponsor-category router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sponsor-category.sponsor-category');
