9583b41eebf40d7b85b7e16cd9455e99ff05e15e
Amelie Benoit <amelie.benoit@busbud.com>
2022-07-12 09:15:23 +0200

    test: env var

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,16 +1,17 @@
+// process.env.API_URL
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
   server: {
-    port: 3615,
+    port: 3615
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "BDX I/O",
     htmlAttrs: {
-      lang: "fr",
+      lang: "fr"
     },
     meta: [
       { charset: "utf-8" },
@@ -18,70 +19,66 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
       { name: "msapplication-TileColor", content: "#da532c" },
-      { name: "theme-color", content: "#ffffff" },
+      { name: "theme-color", content: "#ffffff" }
     ],
     link: [
       {
         rel: "apple-touch-icon",
         sizes: "180x180",
-        href: "/favicons/apple-touch-icon.png",
+        href: "/favicons/apple-touch-icon.png"
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "32x32",
-        href: "/favicons/favicon-32x32.png",
+        href: "/favicons/favicon-32x32.png"
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "16x16",
-        href: "/favicons/favicon-16x16.png",
+        href: "/favicons/favicon-16x16.png"
       },
       {
         rel: "manifest",
-        href: "/favicons/site.webmanifest",
+        href: "/favicons/site.webmanifest"
       },
       {
         rel: "mask-icon",
         href: "/favicons/safari-pinned-tab.svg",
-        color: "#5bbad5",
-      },
-    ],
+        color: "#5bbad5"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/styles/main.scss"],
 
   styleResources: {
-    scss: [
-      "~/assets/styles/_variables.scss",
-      "~/assets/styles/_functions.scss",
-      "~/assets/styles/_mixin.scss",
-    ],
+    scss: ["~/assets/styles/_variables.scss", "~/assets/styles/_functions.scss", "~/assets/styles/_mixin.scss"]
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/featureFlag.client.js", '~/plugins/axios.js'],
+  plugins: ["~/plugins/featureFlag.client.js", "~/plugins/axios.js"],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
     {
       path: "~/components", // will get any components nested in let's say /components/test too
-      pathPrefix: false,
-    },
+      pathPrefix: false
+    }
   ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    "@nuxtjs/eslint-module",
+    "@nuxtjs/eslint-module"
   ],
 
   fontawesome: {
     icons: {
       solid: true,
-      brands: true,
-    },
+      brands: true
+    }
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -92,10 +89,10 @@ export default {
 
   axios: {
     proxy: true,
-    credentials: 'same-origin',
+    credentials: "same-origin",
     withCredentials: true,
     headers: {
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
+      Authorization: `Bearer ${process.env.API_TOKEN}`
     }
   },
 
@@ -109,6 +106,6 @@ export default {
 
   publicRuntimeConfig: {
     newsletterUrl: process.env.NEWSLETTER_URL ?? null,
-    cmsBaseUrl: process.env.CMS_BASE_URL ?? null,
-  },
-}
+    cmsBaseUrl: process.env.CMS_BASE_URL ?? null
+  }
+};
