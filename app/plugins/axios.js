9ea55720cf4582f0744db19c1d2f0c46826d818f
HubM <hubmoncenis@gmail.com>
2022-07-12 08:34:31 +0200

    :art: test env var

--- app/plugins/axios.js
+++ app/plugins/axios.js
@@ -1,4 +1,5 @@
 export default function ({ $axios }) {
+  console.log("process.env", process.env)
   $axios.onRequest((config) => {
     if (process.server) {
       config.url = config.url.replace('/api/', process.env.API_URL)
