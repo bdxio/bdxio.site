5381aad3d6c1fbfa933985a6f24e2aad7c4ce063
HubM <hubmoncenis@gmail.com>
2022-11-21 09:57:01 +0100

    🎨 fetch data + display it w/ responsive

--- app/pages/Sponsors2022.vue
+++ app/pages/Sponsors2022.vue
@@ -3,15 +3,75 @@
     <header class="section-sponsors-2022__header">
       <section-title tag="h1" class="section-sponsors-2022__header__title">Nos sponsors 2022</section-title>
     </header>
+
+    <section class="section-sponsors-2022__body">
+      <ul v-if="offers.length">
+        <li v-for="(offer, index) in offers" :key="`offer-${offer.id}`" class="offer" :class="getOfferClass(index)">
+          <section-title tag="h2" class="offer__name">{{ offer.label }}</section-title>
+          <ul v-if="offer.sponsors?.data.length" class="sponsors">
+            <li v-for="sponsor in offer.sponsors.data" :key="sponsor.name" class="sponsors__sponsor card">
+              <a :href="sponsor.url || '#'" target="_blank" class="sponsors__sponsor__link" :title="sponsor.name">
+                <img
+                  class="sponsors__sponsor__image"
+                  :src="
+                    sponsor.logo && sponsor.logo.data
+                      ? sponsor.logo.data.attributes.formats.thumbnail.url
+                      : 'https://www.bdxio.fr/_nuxt/img/bdxio_logo_blue.7a3769d.png'
+                  "
+                  :alt="`Logo de ${sponsor.name}`"
+                />
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p v-else>Pas de sponsors...</p>
+    </section>
   </main>
 </template>
-
 <script>
+import { formatStrapiData, shuffleArray } from "~/helpers";
+
 export default {
   name: "Sponsors2022Page",
   layout: "page",
   head() {
     return { title: "Sponsors 2022 | BDX I/O" };
+  },
+  data() {
+    return {
+      offers: []
+    };
+  },
+  methods: {
+    getOfferClass(index) {
+      const colors = ["purple", "green", "yellow", "orange"];
+      return `${colors[index]}`;
+    }
+  },
+  async asyncData({ $axios, $config, error, $showSponsors2022 }) {
+    if (!$showSponsors2022) {
+      return error({ statusCode: 404 });
+    }
+
+    const offers = await $axios.$get(`${$config.cmsApiUrl}/offers`, {
+      params: { sort: "id:asc", "populate[sponsors][populate]": "*" }
+    });
+
+    if (!offers) return;
+
+    const offersWithshuffledSponsors = formatStrapiData(offers.data).map((offer) => {
+      if (!offer.sponsors.data.length) {
+        return offer;
+      }
+
+      offer.sponsors.data = shuffleArray(formatStrapiData(offer.sponsors.data));
+      return offer;
+    });
+
+    return {
+      offers: offersWithshuffledSponsors
+    };
   }
 };
 </script>
@@ -65,6 +125,82 @@ export default {
           width: 120px;
           height: 120px;
           right: -110px;
+        }
+      }
+    }
+  }
+
+  &__body {
+    margin-top: 100px;
+    .sponsors {
+      display: flex;
+      grid-gap: 30px;
+      flex-wrap: wrap;
+      // overflow-x: auto;
+      // overflow-y: hidden;
+      align-self: center;
+      padding: $spc-m;
+
+      &__sponsor {
+        min-width: 180px;
+        min-height: 180px;
+        display: block;
+        margin: 0 auto;
+
+        &__link {
+          display: block;
+          width: 100%;
+          height: 100%;
+          position: relative;
+          @include positionRelative;
+        }
+
+        &__image {
+          width: 50%;
+          @include positionAbsolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        @include mobileFirst(s) {
+          min-width: 210px;
+          min-height: 210px;
+          margin: initial;
+        }
+      }
+    }
+
+    .offer {
+      margin-bottom: $spc-xl;
+      &__name {
+        font-size: 20px;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        font-weight: $font-weight-bold;
+      }
+
+      &.purple {
+        .offer__name {
+          color: $purple;
+        }
+      }
+
+      &.green {
+        .offer__name {
+          color: $green;
+        }
+      }
+
+      &.yellow {
+        .offer__name {
+          color: $yellow;
+        }
+      }
+
+      &.orange {
+        .offer__name {
+          color: $orange;
         }
       }
     }
