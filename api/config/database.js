3a14916e763e3776e417c3d3235f32fef07e4002
Antoine Chassagne <antoine.chassagne@me.com>
2023-02-07 13:50:33 +0100

    chore: add knex config option to allow remote database connection

--- api/config/database.js
+++ api/config/database.js
@@ -1,14 +1,19 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-    },
-    debug: false,
-  },
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "0000"),
+        schema: env("DATABASE_SCHEMA", "public"), // Not required
+      },
+      pool: {
+        min: 1,
+        max: 10,
+        propagateCreateError: false
+      },
+      debug: false,
+    }
 });
