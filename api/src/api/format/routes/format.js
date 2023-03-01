5d4047e8d4d6d892da26b0da7a562cd007f36c01
Antoine Chassagne <antoine.chassagne@me.com>
2023-03-01 21:09:54 +0100

    refactor: remove api

--- api/src/api/format/routes/format.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * format router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::format.format');
--- api/src/api/slot/services/slot.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * slot service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::slot.slot');
