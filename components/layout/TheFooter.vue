c228ef137d30a2a57636ca4b212f8c5667974890
HubM <hubmoncenis@gmail.com>
2023-03-08 10:23:56 +0100

    ♻️ footer nuxt 3

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -1,10 +1,43 @@
+<script setup lang="ts">
+import { ref, computed, inject } from "#imports";
+
+const email = ref("");
+const showEmailForm = ref(false);
+
+const disabledButton = computed(() => {
+  if (email.value === "") {
+    return false;
+  }
+
+  return validateEmail(email.value);
+});
+
+const showNavigation = computed(() => inject("showNavigation") ?? false);
+
+const validateEmail = (email: string) =>
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+const registerNewsletter = () => {
+  if (email.value === "" || !validateEmail(email.value)) {
+    return;
+  }
+};
+</script>
+
 <template>
   <div class="footer">
     <section-guidage />
     <footer class="section">
       <div class="bdxio">
         <nuxt-link class="logo" to="/">
-          <img src="~/assets/img/bdxio_logo_blue.png" alt="Logo de l'association BDX.IO" />
+          <img
+            src="~/assets/img/bdxio_logo_blue.png"
+            alt="Logo de l'association BDX.IO"
+          />
           <span>Bordeaux Developers eXperience</span>
         </nuxt-link>
       </div>
@@ -12,7 +45,9 @@
         <div class="location">
           <a href="https://goo.gl/maps/aScUymM4T4DqeBvz8" target="_blank">
             <span class="location-name">Palais des congrès</span><br />
-            <span class="location-address">Avenue Jean Gabriel Domergue<br />33300 BORDEAUX</span>
+            <span class="location-address"
+              >Avenue Jean Gabriel Domergue<br />33300 BORDEAUX</span
+            >
           </a>
         </div>
         <ul class="contact">
@@ -25,28 +60,54 @@
         </ul>
         <ul class="social">
           <li>
-            <a href="https://www.linkedin.com/company/10651416/" target="_blank">
-              <img src="~/assets/img/socials/linkedin.svg" alt="Icône LinkedIn BDX IO" />
+            <a
+              href="https://www.linkedin.com/company/10651416/"
+              target="_blank"
+            >
+              <img
+                src="~/assets/img/socials/linkedin.svg"
+                alt="Icône LinkedIn BDX IO"
+              />
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/bdxio" target="_blank" aria-label="Twitter BDX I/O">
-              <img src="~/assets/img/socials/twitter.svg" alt="Icône Twitter BDX IO" />
+            <a
+              href="https://twitter.com/bdxio"
+              target="_blank"
+              aria-label="Twitter BDX I/O"
+            >
+              <img
+                src="~/assets/img/socials/twitter.svg"
+                alt="Icône Twitter BDX IO"
+              />
             </a>
           </li>
           <li>
-            <a href="https://www.youtube.com/channel/UCA7pEYY0BlgCdpbnjhCDezQ" target="_blank">
-              <img src="~/assets/img/socials/youtube.svg" alt="Icône Youtube BDX IO" />
+            <a
+              href="https://www.youtube.com/channel/UCA7pEYY0BlgCdpbnjhCDezQ"
+              target="_blank"
+            >
+              <img
+                src="~/assets/img/socials/youtube.svg"
+                alt="Icône Youtube BDX IO"
+              />
             </a>
           </li>
         </ul>
         <div class="links" v-if="showNavigation">
           <the-navigation />
         </div>
-        <ul class="newsletter" v-if="$config.newsletterUrl">
+        <ul class="newsletter">
           <li>
-            <span @click="showEmailForm = true" class="cursor--pointer">S'incrire à la newsletter</span>
-            <form v-if="showEmailForm" class="newsletter__form" autocomplete="off" @submit.prevent="registerNewsletter">
+            <span @click="showEmailForm = true" class="cursor--pointer"
+              >S'incrire à la newsletter</span
+            >
+            <form
+              v-if="showEmailForm"
+              class="newsletter__form"
+              autocomplete="off"
+              @submit.prevent="registerNewsletter"
+            >
               <img
                 src="~/assets/img/icons/close_blue.svg"
                 alt="icone pour fermer le formulaire d'ajout d'email à la newsletter"
@@ -55,15 +116,20 @@
               />
               <label for="email" class="label">Mon email :</label>
               <input
-                v-model="mail"
+                v-model="email"
                 class="input"
-                :class="{ valid: validateEmail(mail) }"
+                :class="{ valid: validateEmail(email) }"
                 name="email"
                 placeholder="hello@email.com"
                 size="30"
                 required
               />
-              <button class="button" :class="{ disabled: !disabledButton }" type="submit" :disabled="!mail">
+              <button
+                class="button"
+                :class="{ disabled: !disabledButton }"
+                type="submit"
+                :disabled="!email"
+              >
                 S'inscrire
               </button>
             </form>
@@ -81,86 +147,3 @@
     </footer>
   </div>
 </template>
-
-<script>
-import { mapActions } from "vuex";
-import jsonp from "jsonp";
-
-export default {
-  name: "TheFooter",
-  data() {
-    return {
-      mail: null,
-      showEmailForm: false
-    };
-  },
-  computed: {
-    disabledButton() {
-      if (!this.mail) {
-        return false;
-      }
-
-      return this.validateEmail(this.mail);
-    },
-    showNavigation() {
-      return this.$showNavigation ?? false;
-    }
-  },
-  methods: {
-    ...mapActions({
-      addToast: "toaster/addToast"
-    }),
-    registerNewsletter() {
-      if (!this.mail || !this.validateEmail(this.mail)) {
-        return;
-      }
-
-      const url = `${this.$config.newsletterUrl}&EMAIL=${this.mail}`;
-
-      jsonp(
-        url,
-        {
-          param: "c"
-        },
-        (error, data) => {
-          if (error) {
-            this.addToast({
-              type: error.result,
-              message:
-                error.msg ||
-                "Une erreur est survenue lors de l'inscription de votre email à la newsletter. Merci de réessayer ultérieurement"
-            });
-
-            return;
-          }
-
-          this.addToast({
-            type: data.result,
-            message:
-              data.msg ||
-              "Une erreur est survenue lors de l'inscription de votre email à la newsletter. Merci de réessayer ultérieurement"
-          });
-
-          if (data.result !== "error") {
-            setTimeout(() => {
-              this.mail = null;
-              this.showEmailForm = false;
-            }, 1000);
-          }
-        }
-      );
-    },
-    validateEmail(email) {
-      if (!email) {
-        return false;
-      }
-
-      return String(email)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    }
-  }
-};
-</script>
