3f1e26b3b299a58528621b7d7676b6588d8fda2d
Antoine Chassagne <antoine.chassagne@me.com>
2023-03-10 14:19:28 +0100

    refactor: make Sponsors page compatible with Nuxt 3

--- pages/Sponsors.vue
+++ pages/Sponsors.vue
@@ -1,64 +1,43 @@
+<script setup lang="ts">
+import { definePageMeta, useHead, useFetch, computed, inject } from "#imports";
+import { formatStrapiData, shuffleArray } from "~/utils";
+import { useConfig } from "~/composables";
+
+definePageMeta({ layout: "page" });
+
+useHead({
+  title: "Sponsors | BDX I/O",
+});
+
+const { API_URL } = useConfig();
+
+const showSponsors2022 = inject("showSponsors2022");
+
+if (!showSponsors2022) {
+  throw createError({ statusCode: 404, statusMessage: "Page not found" });
+}
+
+const { data } = await useFetch(`${API_URL}/api/item`, {
+  params: { sort: "id:asc", "populate[sponsors][populate]": "*" },
+});
+
+const showSponsors = computed(() => data?.value?.length && showSponsors2022);
+
+const offers = formatStrapiData(data.value.data).map((offer) => {
+  if (offer.sponsors?.data?.length) {
+    offer.sponsors.data = shuffleArray(formatStrapiData(offer.sponsors.data));
+  }
+  return offer;
+});
+</script>
+
 <template>
   <main>
     <section-become-sponsor />
-    <section-sponsor-offers-and-sponsors v-if="showSponsors && offers" :offers="offers" />
+    <section-sponsor-offers-and-sponsors
+      v-if="showSponsors && offers"
+      :offers="offers"
+    />
     <section-sponsor-offers v-else />
   </main>
 </template>
-
-<script>
-import SectionBecomeSponsor from "~/components/sponsors/SectionBecomeSponsor.vue";
-import SectionSponsorOffersAndSponsors from "~/components/sponsors/SectionSponsorOffersAndSponsors.vue";
-
-import SectionSponsorOffers from "~/components/sponsors/SectionSponsorOffers.vue";
-
-import { formatStrapiData, shuffleArray } from "~/utils";
-export default {
-  name: "SponsorsPage",
-  layout: "page",
-  data() {
-    return {
-      offers: []
-    };
-  },
-  head() {
-    return {
-      title: "Sponsors | BDX I/O"
-    };
-  },
-  components: {
-    SectionBecomeSponsor,
-    SectionSponsorOffersAndSponsors,
-    SectionSponsorOffers
-  },
-  computed: {
-    showSponsors() {
-      return this.$showSponsors2022 && this.offers.length;
-    }
-  },
-  async asyncData({ $axios, $config, error, $showSponsors2022 }) {
-    if (!$showSponsors2022) {
-      return error({ statusCode: 404 });
-    }
-
-    const offers = await $axios.$get(`${$config.cmsApiUrl}/offers`, {
-      params: { sort: "id:asc", "populate[sponsors][populate]": "*" }
-    });
-
-    if (!offers) return;
-
-    const offersWithshuffledSponsors = formatStrapiData(offers.data).map((offer) => {
-      if (!offer.sponsors.data.length) {
-        return offer;
-      }
-
-      offer.sponsors.data = shuffleArray(formatStrapiData(offer.sponsors.data));
-      return offer;
-    });
-
-    return {
-      offers: offersWithshuffledSponsors
-    };
-  }
-};
-</script>
