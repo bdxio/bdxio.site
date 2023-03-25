47dfe1ed7efb923c47733f5e5b14a2c03ab8ed28
HubM <hubmoncenis@gmail.com>
2023-03-25 12:44:00 +0100

    ♻️  SectionHomepageSponsor tailwind

--- components/section/homepage/Sponsor.vue
+++ components/section/homepage/Sponsor.vue
@@ -1,37 +1,38 @@
 <script setup lang="ts">
-// @ts-nocheck
-import { SectionTitle } from "#components";
+import { SectionTitle, ButtonSecondary } from "#components";
 </script>
 
 <template>
-  <section class="section section-sponsor-call" id="become-sponsor">
+  <section
+    id="become-sponsor"
+    class="section flex flex-col justify-between items-start bg-bdxio-blue-light text-bdxio-font-light"
+  >
     <div>
-      <SectionTitle section tag="h2" color="light">sponsors</SectionTitle>
-      <SectionTitle tag="h3" class="title" color="light">
+      <SectionTitle
+        section
+        tag="h2"
+        color="light"
+      >
+        sponsors
+      </SectionTitle>
+      <SectionTitle
+        tag="h3"
+        class="title"
+        color="light"
+      >
         Embarquez avec nous dans l'aventure&nbsp;!
       </SectionTitle>
       <p>
-        Vous souhaitez devenir sponsor de cette nouvelle édition&nbsp;? <br />
+        Vous souhaitez devenir sponsor de cette nouvelle édition&nbsp;? <br>
         En cliquant sur le bouton ci-dessous vous trouverez toutes les
         informations nécessaires.
       </p>
     </div>
-    <button
+    <ButtonSecondary
+      color="grey"
       disabled
-      class="button button-secondary button-secondary--grey disabled"
     >
       Non disponible
-    </button>
+    </ButtonSecondary>
   </section>
 </template>
-
-<style lang="scss" scoped>
-.section-sponsor-call {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  background-color: $primary-light;
-  color: $light-font;
-}
-</style>
