ee276091f4cd2dadeba370899b7d00c6525b272c
Antoine Chassagne <antoine.chassagne@me.com>
2023-03-29 19:29:57 +0200

    chore: add lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
+const OFF = 0;
+const ERROR = 2;
+
 module.exports = {
   root: true,
   extends: ["@nuxt/eslint-config"],
   rules: {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "comma-dangle": ["error", "always-multiline"],
-    "max-len": ["error", { "code": 120, "tabWidth": 2,  "ignoreUrls": true, "ignoreStrings": true, "ignoreTemplateLiterals": true, "ignoreRegExpLiterals": true }],
-    "vue/multi-word-component-names": "off",
+    "semi": [ERROR, "always"],
+    "quotes": [ERROR, "double"],
+    "indent": [ERROR, 2, {
+      "SwitchCase": 1,
+    }],
+    "comma-dangle": [ERROR, "always-multiline"],
+    "max-len": [ERROR, { "code": 120, "tabWidth": 2, "ignoreUrls": true, "ignoreStrings": true, "ignoreTemplateLiterals": true, "ignoreRegExpLiterals": true }],
+    "vue/multi-word-component-names": OFF,
+    "keyword-spacing": [2, { "before": true, "after": true }],
   },
 };
