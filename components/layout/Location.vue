a2a1f47fdcbfac4ef4f7e879bad359023662e33f
Antoine Chassagne <antoine.chassagne@me.com>
2023-04-10 19:06:19 +0200

    chore: remove unecessary classes

--- components/layout/Location.vue
+++ components/layout/Location.vue
@@ -11,37 +11,34 @@ import { Heading, HeadingSection } from "#components";
       <div class="bg-grey-100 s:max-w-[50%] shadow-illustration p-0 m:p-8">
         <HeadingSection
           level="2"
-          class="text-subtitle !text-bdxio-blue-dark"
+          class="text-subtitle"
         >
           Transports
         </HeadingSection>
-        <Heading
-          level="3"
-          class="!text-bdxio-blue-dark"
-        >
+        <Heading level="3">
           Comment rejoindre<br>l'évènement ?
         </Heading>
         <div class="flex flex-col gap-1 mb-[30px]">
-          <span class="font-bold text-2xl text-bdxio-blue-dark">Palais des Congrès</span>
-          <span class="text-bdxio-blue-dark">Avenue Jean Gabriel Domergue<br>33300 BORDEAUX</span>
+          <span class="font-bold text-2xl">Palais des Congrès</span>
+          <span>Avenue Jean Gabriel Domergue<br>33300 BORDEAUX</span>
         </div>
         <div>
-          <span class="font-bold text-2xl text-bdxio-blue-dark">Transport en commun :
+          <span class="font-bold text-2xl">Transport en commun :
           </span>
-          <span class="text-bdxio-blue-dark">Bus 15 ou Tram C</span>
+          <span>Bus 15 ou Tram C</span>
         </div>
         <div>
-          <span class="font-bold text-2xl text-bdxio-blue-dark">Vélo : </span>
-          <span class="text-bdxio-blue-dark">
+          <span class="font-bold text-2xl">Vélo : </span>
+          <span>
             Parking vélo disponible et station VCub à proximité
           </span>
         </div>
         <div>
-          <span class="font-bold text-2xl text-bdxio-blue-dark">Voiture : </span>
-          <span class="text-bdxio-blue-dark">Parking gratuit sur place</span>
+          <span class="font-bold text-2xl">Voiture : </span>
+          <span>Parking gratuit sur place</span>
         </div>
       </div>
-      <span class="italic absolute right-0 top-0 hidden m:block !text-bdxio-blue-dark">
+      <span class="italic absolute right-0 top-0 hidden m:block">
         À seulement 10 min de Bordeaux !
       </span>
     </div>
