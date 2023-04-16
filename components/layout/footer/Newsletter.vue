3abe65ec641f4ddaf404c2595f66173a824ae876
Antoine Chassagne <antoine@poool.fr>
2023-04-16 15:32:32 +0200

    feat: add newsletter registration

--- components/layout/footer/Newsletter.vue
+++ components/layout/footer/Newsletter.vue
@@ -1,75 +1,142 @@
 <script setup lang="ts">
-import { ref, computed } from "#imports";
+import { reactive, useRuntimeConfig, useFetch } from "#imports";
+import { ButtonSecondary, Loader } from "#components";
 
-const email = ref("");
-const formOpen = ref(false);
-
-const isEmailValid = computed(() => {
-  return email.value?.trim()?.toLowerCase().match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  );
+const state = reactive({
+  open: false,
+  valid: false,
+  loading: false,
+  success: false,
+  error: false,
+  email: "",
 });
 
-const toggleForm = () => {
-  formOpen.value = !formOpen.value;
-};
+const config = useRuntimeConfig();
 
-const register = () => {
-  if (!isEmailValid.value) {
+function toggleForm() {
+  state.open = !state.open;
+}
+
+function onType(event: Event) {
+  resetFeedback();
+  state.error = false;
+
+  const target = event.target as HTMLInputElement;
+  state.valid = validateEmail(target.value);
+}
+
+function validateEmail(value: string) {
+  return (
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  ).test(value?.trim()?.toLowerCase());
+}
+
+function resetFeedback() {
+  state.error = false;
+  state.success = false;
+}
+
+async function register() {
+  if (!validateEmail(state.email)) {
     return;
   }
-  // @TODO : Implement registration
-};
+  
+  state.loading = true;
+
+  if (!config.public.NEWSLETTER_URL) {
+    console.error("Missing NEWSLETTER_URL environment variable");
+    state.error = true;
+    state.loading = false;
+    return;
+  }
+
+  const { error: requestError } = await useFetch(
+    `${config.public.NEWSLETTER_URL}&EMAIL=${state.email}`,
+    { method: "POST" },
+  );
+
+  if (requestError) {
+    state.error = true;
+    state.loading = false;
+    return;
+  }
+
+  state.success = true;
+  state.loading = false;
+}
 </script>
 
 <template>
-  <div class="flex">
-    <span
-      class="cursor-pointer"
-      @click="toggleForm"
-    >
-      S'incrire à la newsletter
-    </span>
+  <div
+    class="flex items-center"
+    @click="toggleForm"
+  >
+    <span class="cursor-pointer">S'incrire à la newsletter</span>
     <img
       src="/images/drawings/purple_envelope.png"
       class="w-16 m:w-24 cursor-pointer"
-      @click="toggleForm"
     >
   </div>
-  <div>
+  <div
+    v-if="state.open"
+    class="fixed top-0 left-0 bg-bdxio-blue-dark w-screen h-screen flex flex-col items-center justify-center z-20"
+  >
+    <img
+      src="/images/icons/close.svg"
+      alt="Icône croix"
+      class="absolute top-8 m:top-12 right-8 m:right-16 cursor-pointer"
+      @click="toggleForm"
+    >
+    <p
+      v-if="state.success"
+      class="text-white"
+    >
+      Votre inscription est validée !
+    </p>
     <form
-      v-if="formOpen"
-      autocomplete="off"
+      v-else
+      class="flex flex-col m:justify-center w-full m:w-auto p-4"
       @submit.prevent="register"
     >
-      <img
-        src="/images/icons/close_blue.svg"
-        alt="Icône croix"
-        @click="toggleForm"
-      >
-      <label
-        for="email"
-        class="label"
-      >
-        Adresse email :
-      </label>
-      <input
-        v-model="email"
-        class="input"
-        :class="{ valid: isEmailValid }"
-        name="email"
-        placeholder="hello@email.com"
-        size="30"
-        required
-      >
-      <button
-        class="button"
-        :class="{ disabled: !isEmailValid }"
-        :disabled="!email"
-        type="submit"
-      >
-        S'inscrire
-      </button>
+      <Loader
+        v-if="state.loading"
+        color="#FFFFFF"
+        :width="100"
+        :height="100"
+        class="m-auto"
+      />
+      <template v-else>
+        <p class="text-white text-3xl m:text-4xl mb-4">
+          S'inscrire à la newsletter
+        </p>
+        <div class="flex flex-col m:flex-row m:justify-center">
+          <input
+            v-model="state.email"
+            name="email"
+            placeholder="Saisissez votre adresse email"
+            size="30"
+            required
+            :class="{ valid: state.valid }"
+            class="py-3 px-6 rounded-xl border-solid border-2"
+            @input="onType"
+          >
+          <ButtonSecondary
+            color="white"
+            :disabled="!state.valid"
+            type="submit"
+            :class="{ disabled: !state.valid }"
+            class="m:ml-4 mt-4 m:mt-0"
+          >
+            S'inscrire
+          </ButtonSecondary>
+        </div>
+        <p
+          v-if="state.error"
+          class="text-red pt-3"
+        >
+          Une erreur est survenue. <br>Merci de réessayer dans quelques minutes.
+        </p>
+      </template>
     </form>
   </div>
 </template>
