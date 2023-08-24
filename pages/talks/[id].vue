32f1a6b1706285281204a1dc48cd225353d227a9
HubM <hubmoncenis@gmail.com>
2023-08-24 17:31:54 +0200

    ⏪️ enlever l'ancre

--- pages/talks/[id].vue
+++ pages/talks/[id].vue
@@ -29,7 +29,6 @@ if (!talk.value) {
     <SectionTalkPresentation :talk="talk" />
     <SectionTalkSpeaker
       v-for="speaker in talk.speakers"
-      id="speaker"
       :key="speaker.id"
       :speaker="speaker"
     />
