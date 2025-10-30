1f6989eaa668dadfe8f96038f50a6530d6b310ee
Sébastien Moreno <sebastien.moreno@gmail.com>
2025-10-30 19:26:14 +0100

    fixup! feat: Bluesky Feed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   // Ajout pour Server API: Bluesky feed fetching
   nitro: {
-    preset: 'netlify'
+    preset: 'netlify',
+    prerender: {
+      routes: ['/'] // uniquement les pages nécessaires
+    }
   },
 
   devServer: {
