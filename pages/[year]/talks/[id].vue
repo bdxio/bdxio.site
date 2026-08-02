617f6fb8c25e3ad6b84347813827f54f81084947
Maxime Cousinou <maxime.cousinou@zemax.net>
2026-08-02 19:04:08 +0200

    fix(schedule): cleanup remaining link in archives

--- pages/[year]/talks/[id].vue
+++ pages/[year]/talks/[id].vue
@@ -8,8 +8,6 @@ useHead({ title: `Talk | ${ASSOCIATION_NAME}` });
 
 const { params }: { params: Record<string, string> } = useRoute();
 
-const { $featureFlags } = useNuxtApp();
-
 const { data: talk }: { data: Ref<Talk> } = await useAPI(`/talks/${params.id}`, {
   params: { populate: '*' },
 });
@@ -17,9 +15,6 @@ const { data: talk }: { data: Ref<Talk> } = await useAPI(`/talks/${params.id}`, 
 if (!talk.value || talk.value?.edition?.year !== edition || talk.value?.type === CLOSING_TALK_TYPE) {
   throw createError({ statusCode: 404, statusMessage: 'Page not found' });
 }
-
-const route = useRoute('year');
-const year = route.params.year as string;
 </script>
 
 <template>
@@ -27,14 +22,5 @@ const year = route.params.year as string;
     <SectionTalkCategory v-if="talk.category" :category="talk.category" />
     <SectionTalkPresentation :talk="talk" />
     <SectionTalkSpeaker v-for="speaker in talk.speakers" :key="speaker.id" :speaker="speaker" />
-    <LinkSecondary
-      v-if="$featureFlags.pages.schedule.show"
-      color="light"
-      :to="`/${year}/schedule`"
-      type="link"
-      class="block mt-5 mx-auto mb-0"
-    >
-      Voir le programme
-    </LinkSecondary>
   </main>
 </template>
